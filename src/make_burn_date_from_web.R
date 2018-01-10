d406d45fc944c0a69fc5a3ebcb3498ec0ea35258
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-01-10 08:57:14 -0700

    parallelized code

--- src/make_burn_date_from_web.R
+++ src/make_burn_date_from_web.R
@@ -1,66 +1,45 @@
-###################################################
-# This script creates a layer of burn date for the#
-# entire USA from MODIS collection 6 Burned area  #
-# product (MCD64)                                 #
-# Author: Adam Mahood                             #
-# Latest Modification: March 2017                 #
-###################################################
 
 # Download from:
 # ftp://fuoco.geog.umd.edu/MCD64A1/C6/
 # username: fire
 # password: burnt
 
-# filenames from subdatastes
-## "HDF4_EOS:EOS_GRID:MCD64A1.A2014213.h10v04.051.2014284031847.hdf:MOD_Grid_Monthly_500m_DB_BA:Burn Date" 
-# [1] "HDF4_EOS:EOS_GRID:MCD64A1.A2014213.h10v04.051.2014284031847.hdf:MOD_Grid_Monthly_500m_DB_BA:Burn Date"            
-# [2] "HDF4_EOS:EOS_GRID:MCD64A1.A2014213.h10v04.051.2014284031847.hdf:MOD_Grid_Monthly_500m_DB_BA:Burn Date Uncertainty"
-# [3] "HDF4_EOS:EOS_GRID:MCD64A1.A2014213.h10v04.051.2014284031847.hdf:MOD_Grid_Monthly_500m_DB_BA:QA"                   
-# [4] "HDF4_EOS:EOS_GRID:MCD64A1.A2014213.h10v04.051.2014284031847.hdf:MOD_Grid_Monthly_500m_DB_BA:First Day"            
-# [5] "HDF4_EOS:EOS_GRID:MCD64A1.A2014213.h10v04.051.2014284031847.hdf:MOD_Grid_Monthly_500m_DB_BA:Last Day" 
-
 library(MODIS)
 library(tidyverse)
 library(raster)
 library(RCurl)
 library(gdalUtils)
+library(foreach)
+library(doParallel)
 
-top_directory = "data/MODIS/"
-output_directory = "data/MODIS/output/"
-final_output = "data/MODIS/USA_BURNDATE/"
+top_directory = file.path("data", "MCD64A1", "C6")
+output_directory = file.path("data", "MCD64A1", "C6", "yearly_tiles")
+final_output = file.path("data", "MCD64A1", "C6", "usa_burndate")
 
-#optional
-dir.create(paste0(top_directory), recursive = TRUE)
-dir.create(paste0(output_directory), recursive = TRUE)
-dir.create(paste0(final_output), recursive = TRUE)
-
+# Check if directory exists for all variable aggregate outputs, if not then create
+var_dir <- list(top_directory, output_directory, final_output)
+lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
 
 url = "ftp://fire:burnt@fuoco.geog.umd.edu/MCD64A1/C6/"
 u_p = "fire:burnt"
 
 # Reclassification matrix to remove NA values
 mtrx = matrix(c(-Inf, 0, 0, 367, Inf, 0), byrow=TRUE, ncol=3)
-
-tiles = c("h08v04", 
-          "h08v05", 
-          "h09v04", 
-          "h09v05", 
-          "h10v04",
-          "h10v05",
-          "h10v06",
-          "h11v04",
-          "h11v05",
-          "h12v04",
-          "h12v05",
-          "h13v04")
+tiles = c("h08v04", "h08v05", "h09v04", "h09v05", "h10v04", "h10v05",
+          "h10v06", "h11v04", "h11v05", "h12v04", "h12v05", "h13v04")
 
 names <- c("BurnDate", "BurnDateUncertainty")
-
 layers <- c("Burn Date", "Burn Date Uncertainty", "QA", "First Day", "Last Day")
 
-for(j in 1:length(tiles)){
+#setup parallel backend to use many processors
+cores=detectCores()
+cl <- makeCluster(cores)
+
+# Download all tiles for the conterminous US
+registerDoParallel(cl)
+foreach(j = 1:length(tiles)) %dopar% {
   dir.create(paste0(top_directory, tiles[j]), recursive = TRUE)
-  filenames <- getURL(paste0(url,tiles[j],"/"), userpwd = u_p, v=T, ftp.use.epsv = FALSE, dirlistonly = TRUE)
+  filenames <- RCurl::getURL(paste0(url,tiles[j],"/"), userpwd = u_p, v=T, ftp.use.epsv = FALSE, dirlistonly = TRUE)
   filenames = paste0(strsplit(filenames, "\r*\n")[[1]])
   for(L in 1:length(filenames)){
     output_file_name <- file.path(paste0(top_directory,tiles[j]), filenames[L])
@@ -70,11 +49,12 @@ for(j in 1:length(tiles)){
     } 
   }
 }
+stopCluster(cl)
 
-# next, take the burn date rasters out of the .hdfs
-for(d in 1:2){ 
-  for(j in 1:length(tiles)){    
-    
+# Take the burn date rasters out of the .hdfs and convert to .tif files
+registerDoParallel(cl)
+foreach (d = 1:2) %:% # nesting operator
+  foreach (j = 1:length(tiles)) %dopar% {
     hdfs = list.files(paste0(top_directory, tiles[j]), pattern = ".hdf", 
                       recursive = TRUE)
     
@@ -95,8 +75,13 @@ for(d in 1:2){
         writeRaster(r, paste0(top_directory, tiles[j], "/", newfilename1[M]))
       }
     }
-    
-    for(i in 2000:2017){
+  }
+stopCluster(cl)
+
+# Create yearly composites for all tiles
+registerDoParallel(cl)
+foreach (j = 1:length(tiles)) %:% # nesting operator
+  foreach (i = 2000:2017) %dopar% {
       tile_files = list.files(paste0(top_directory, tiles[j]), 
                               pattern = "*.tif$", full.names=TRUE)
       if(!file.exists(paste(output_directory, "AllYear_BD_", tiles[j], "_", i, ".tif", sep=""))){
@@ -108,16 +93,12 @@ for(d in 1:2){
         
         writeRaster(fire, tfilename, format = "GTiff")
       }
-      
-    }
-    
     files_to_delete = list.files(paste0(top_directory,tiles[j],"/"))
     file.remove(files_to_delete)
   }
-}
+stopCluster(cl)
 
-######################## Then, stitch them all together
-
+# Then, stitch them all together
 for(d in 1:2) {
   for(k in 2000:2017){
     
