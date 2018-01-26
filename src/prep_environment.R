292c04bb369fe413bc72a434069cf7d204f96695
Adam Mahood <adam.mahood@gmail.com>
2018-01-26 12:00:10 -0700

    l

--- src/prep_environment.R
+++ src/prep_environment.R
@@ -3,10 +3,39 @@
 # ftp://fuoco.geog.umd.edu/MCD64A1/C6/
 # username: fire
 # password: burnt
+
+
+# load the required libraries ---------------
 x <- c("MODIS", "tidyverse", "magrittr", "raster", "RCurl", "gdalUtils", "foreach", "doParallel", "sf")
 lapply(x, library, character.only = TRUE, verbose = FALSE)
 
-p4string_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"   #http://spatialreference.org/ref/sr-org/6903/
+# shapefiles --------------------------------
+# input shapefile of interest
+shp_dsn <- "src/es"          # this is the directory where the shapefile is stored
+shp_layer <- "CBR" 
+get_tiles <- function(dsn, layer){
+  shp_dsn <- dsn          # this is the directory where the shapefile is stored
+  shp_layer <- layer        # this is the pre-extension name of the files associated with the shapfile
+  shp_oi <- st_read(dsn = shp_dsn,
+                    layer = shp_layer, 
+                    quiet= TRUE)
+  
+  # input modis shapefile
+  ms_dsn <- "src/ms"
+  ms_layer <- "modis_sinusoidal_grid_world"
+  shp_ms <- st_read(dsn = ms_dsn,
+                    layer = ms_layer, 
+                    quiet= TRUE)
+  
+  tiles <- shp_oi %>%
+    sf::st_transform(st_crs(shp_ms)) %>%
+    st_intersection(shp_ms) %>%
+    as.data.frame()%>%
+    select(h, v) 
+  tiles <- as.vector()
+}
+
+p4string_ea <- st_crs(shp_oi)
 p4string_ms <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs "
 
 # Raw data folders
@@ -20,7 +49,7 @@ top_directory <- file.path("data", "MCD64A1", "C6")
 hdf_months <- file.path(top_directory, "hdf_months")
 tif_months <- file.path(top_directory, "tif_months")
 tif_year <- file.path(top_directory, "tif_years")
-final_output <- file.path(top_directory, "usa_burndate")
+final_output <- file.path(top_directory, "result")
 
 # Check if directory exists for all variable aggregate outputs, if not then create
 var_dir <- list(prefix, raw_prefix, us_prefix, MCD64A1_dir, hdf_months, tif_months, tif_year, final_output)
@@ -31,8 +60,11 @@ u_p = "fire:burnt"
 
 # Reclassification matrix to remove NA values
 mtrx = matrix(c(-Inf, 0, 0, 367, Inf, 0), byrow=TRUE, ncol=3)
-tiles = c("h08v04", "h08v05", "h09v04", "h09v05", "h10v04", "h10v05",
-          "h10v06", "h11v04", "h11v05", "h12v04", "h12v05", "h13v04")
+# tiles = c("h08v04", "h08v05", "h09v04", "h09v05", "h10v04", "h10v05",
+#           "h10v06", "h11v04", "h11v05", "h12v04", "h12v05", "h13v04")
+
+
+
 
 names <- c("BurnDate")
 layers <- c("Burn Date", "Burn Date Uncertainty", "QA", "First Day", "Last Day")
@@ -40,35 +72,36 @@ layers <- c("Burn Date", "Burn Date Uncertainty", "QA", "First Day", "Last Day")
 #setup parallel backend to use many processors
 cores <- detectCores()
 
-#Download the USA States layer -------------------------
-us_shp <- file.path(us_prefix, "cb_2016_us_state_20m.shp")
-if (!file.exists(us_shp)) {
-  loc <- "https://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip"
-  dest <- paste0(us_prefix, ".zip")
-  download.file(loc, dest)
-  unzip(dest, exdir = us_prefix)
-  unlink(dest)
-  assert_that(file.exists(us_shp))
-}
+# #Download the USA States layer -------------------------
+# us_shp <- file.path(us_prefix, "cb_2016_us_state_20m.shp")
+# if (!file.exists(us_shp)) {
+#   loc <- "https://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip"
+#   dest <- paste0(us_prefix, ".zip")
+#   download.file(loc, dest)
+#   unzip(dest, exdir = us_prefix)
+#   unlink(dest)
+#   assert_that(file.exists(us_shp))
+# }
+# 
+# usa_shp <- st_read(dsn = us_prefix,
+#                    layer = "cb_2016_us_state_20m", quiet= TRUE) %>%
+#   filter(!(STUSPS %in% c("AK", "HI", "PR"))) %>%
+#   dplyr::select(STUSPS) %>%
+#   st_transform(p4string_ea) 
+# names(usa_shp) %<>% tolower
+# 
+# usa_ms <- st_transform(usa_shp, crs = p4string_ms) %>%
+#   as(., "Spatial")
+# 
+# 
+# wus_shp <- st_read(dsn = us_prefix,
+#                    layer = "cb_2016_us_state_20m", quiet= TRUE) %>%
+#   filter(STUSPS %in% c("CO", "WA", "OR", "NV", "CA", "ID", "UT",
+#                        "WY", "NM", "AZ", "MT")) %>%
+#   dplyr::select(STUSPS) %>%
+#   st_transform(p4string_ea) 
+# names(wus_shp) %<>% tolower
+# 
+# wus_ms <- st_transform(wus_shp, crs = p4string_ms) %>%
+#   as(., "Spatial")
 
-usa_shp <- st_read(dsn = us_prefix,
-                   layer = "cb_2016_us_state_20m", quiet= TRUE) %>%
-  filter(!(STUSPS %in% c("AK", "HI", "PR"))) %>%
-  dplyr::select(STUSPS) %>%
-  st_transform(p4string_ea) 
-names(usa_shp) %<>% tolower
-
-usa_ms <- st_transform(usa_shp, crs = p4string_ms) %>%
-  as(., "Spatial")
-
-
-wus_shp <- st_read(dsn = us_prefix,
-                   layer = "cb_2016_us_state_20m", quiet= TRUE) %>%
-  filter(STUSPS %in% c("CO", "WA", "OR", "NV", "CA", "ID", "UT",
-                       "WY", "NM", "AZ", "MT")) %>%
-  dplyr::select(STUSPS) %>%
-  st_transform(p4string_ea) 
-names(wus_shp) %<>% tolower
-
-wus_ms <- st_transform(wus_shp, crs = p4string_ms) %>%
-  as(., "Spatial")
