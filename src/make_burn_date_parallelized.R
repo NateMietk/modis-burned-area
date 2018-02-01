5328768a43341457c266dabc62189b8193f52444
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-02-01 09:51:27 -0700

    add tryCatch

--- src/make_burn_date_parallelized.R
+++ src/make_burn_date_parallelized.R
@@ -1,14 +1,18 @@
+#setup parallel backend to use many processors
+cores <- detectCores()
 
 # Download all .hdf files from ftp site for the Conterminous US
 cl <- makeCluster(cores)
 registerDoParallel(cl)
-foreach(j = 1:length(tiles)) %dopar% {
-  filenames <- RCurl::getURL(paste0(url,tiles[j],"/"), userpwd = u_p, v=T, ftp.use.epsv = FALSE, dirlistonly = TRUE)
+for(j in 1:length(tiles)){
+  url = "ftp://fire:burnt@fuoco.geog.umd.edu/MCD64A1/C6/"
+  u_p = "fire:burnt"
+  filenames <- RCurl::getURL(paste0(url, tiles[j],"/"), userpwd = u_p, v=T, ftp.use.epsv = FALSE, dirlistonly = TRUE)
   filenames = paste0(strsplit(filenames, "\r*\n")[[1]])
   for(L in 1:length(filenames)){
     output_file_name <- file.path(hdf_months, filenames[L])
     if(!file.exists(output_file_name)) {
-      download.file(paste0(url,tiles[j],"/",filenames[L]),
+      download.file(paste0(url, tiles[j],"/",filenames[L]),
                     output_file_name)
     }
   }
@@ -22,7 +26,7 @@ foreach (j = 1:length(tiles)) %dopar% {
   require(magrittr)
   require(raster)
   require(MODIS)
-
+  require(dplyr)
   hdfs = list.files(hdf_months, pattern = ".hdf",
                     recursive = TRUE)
 
@@ -35,9 +39,14 @@ foreach (j = 1:length(tiles)) %dopar% {
 
   hdfs_full = list.files(hdf_months, pattern = ".hdf",
                          recursive = TRUE, full.names = TRUE)
-
+  read.hdfs <- function(file, ...) {
+    res <- tryCatch(MODIS::getSds(file, ...), 
+                    error = function(c) {
+                      c$message <- paste0(c$message, " (in ", file, ")")
+                      warning(c)
+                    } ) }
   for (M in 1:length(hdfs_full)) {
-    sds <- MODIS::getSds(hdfs_full[M])
+    sds <- read.hdfs(hdfs_full[M])
     r <- raster::raster(sds$SDS4gdal[d])
     if(!file.exists(paste0(tif_months, "/", outname[M]))) {
       raster::writeRaster(r, paste0(tif_months, "/", outname[M]), overwrite=TRUE)
@@ -47,14 +56,17 @@ foreach (j = 1:length(tiles)) %dopar% {
 stopCluster(cl)
 
 # Create yearly composites for all tiles
-cl <- makeCluster(cores)
+cl <- makeCluster(cores/2)
 registerDoParallel(cl)
 foreach (j = 1:length(tiles)) %dopar% {
   for(i in 2001:2017) {
     require(magrittr)
     require(raster)
     tile_files = as.vector(Sys.glob(paste0(tif_months, "/", "*", i, "*", tiles[j], ".tif")))
-
+    
+    # Reclassification matrix to remove NA values
+    mtrx = matrix(c(-Inf, 1, 0, 366, Inf, 0), byrow=TRUE, ncol=3)
+    
     if(!file.exists(paste(tif_year, "/Yearly_BD_", tiles[j], "_", i, ".tif", sep=""))){
       fire = raster::stack(tile_files) %>%
         raster::reclassify(., mtrx) %>%
@@ -77,13 +89,13 @@ for(k in 2001:2017) {
       tile_files = as.vector(Sys.glob(paste0(tif_year, "/", "*", k, "*", ".tif")))
       final <- lapply(tile_files, raster)
       final <- do.call(merge, final) %>%
-        raster::crop(usa_ms) %>%
-        raster::mask(usa_ms) %>%
+        raster::crop(wus_ms) %>%
+        raster::mask(wus_ms) %>%
         raster::projectRaster(crs = p4string_ea, res = 500) %>%
-        raster::crop(as(usa_shp, "Spatial")) %>%
-        raster::mask(as(usa_shp, "Spatial"))
+        raster::crop(as(wus_shp, "Spatial")) %>%
+        raster::mask(as(wus_shp, "Spatial"))
 
-      final_name <- paste0(final_output,"/USA_", names, "_", k, ".tif")
+      final_name <- paste0(final_output,"/WUS_", names, "_", k, ".tif")
       raster::writeRaster(final, final_name, format = "GTiff")
     }
   }
