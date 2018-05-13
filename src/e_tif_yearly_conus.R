d0bd37e0aec2d1eaec84b5c2cded19378372e8c5
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-13 06:39:04 -0600

    parallel reproject only

--- src/e_tif_yearly_conus.R
+++ src/e_tif_yearly_conus.R
@@ -1,28 +1,31 @@
 # Create yearly composites for the lower 48 states
 
-cl <- makeCluster(1)
-registerDoParallel(cl)
-
 # Create mosaic of burned area for the lower 48 US for each year
-foreach (i = 2001:2017, .packages = c('foreach', 'tidyverse', 'raster')) %dopar% {
+for (i in 2001:2017) {
 
   if(!file.exists(paste0(yearly_composites,"/USA_", names, "_", i, ".tif"))){
     tile_files = as.vector(Sys.glob(paste0(tif_year, "/", "*", i, ".tif")))
 
     final <- lapply(tile_files, raster) %>%
-      do.call(merge, .) %>%
-      raster::crop(usa_ms) %>%
-      raster::mask(usa_ms) %>%
-      raster::projectRaster(., crs = p4string_ea, res = 500, method = 'ngb') %>%
-      raster::crop(usa) %>%
-      raster::mask(usa)
+      do.call(merge, .)
+
+    library(snow)
+    beginCluster(detectCores()/2)
+
+    final <- final %>%
+      raster::projectRaster(crs = p4string_ea,
+                            res = 500,
+                            method = 'ngb') %>%
+      raster::crop(as(usa, 'Spatial')) %>%
+      raster::mask(as(usa, 'Spatial'))
+
+    endCluster()
 
     final_name <- paste0(yearly_composites,"/USA_", names, "_", i, ".tif")
 
     raster::writeRaster(final, final_name, format = "GTiff", overwrite=TRUE)
 
     system("aws s3 sync data/MCD64A1/C6 s3://earthlab-natem/modis-burned-area/MCD64A1/C6")
+    gc()
   }
 }
-
-stopCluster(cl)
