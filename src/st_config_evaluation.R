726442054336219b08e9282ef32afbbdde302204
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-06-05 10:52:05 -0600

    comment out

--- src/st_config_evaluation.R
+++ src/st_config_evaluation.R
@@ -4,13 +4,18 @@
 st_config <- read_csv(file.path(version_dir, 'confusion_matrix', 'confusion_matrices.csv')) %>%
   left_join(., read_csv(file.path(version_dir, 'confusion_matrix', 'additional_variables_confusion_matrix.csv')), by = 'st_combo') %>%
   separate(st_combo, c('space', 'time'), sep = 3) %>% 
-  mutate(total_mtbs = modisT_mtbsT + modisF_mtbsT,
-         total_modis = modisT_mtbsF_modis_over_th + modisT_mtbsT,
-         proportion = total_modis/total_mtbs *100,
-         modis_to_mtbs_all = modisT_mtbsF_modis_over_th/modisF_mtbsT,
-         modis_to_mtbs_id_1 = num_modisIDs1_and_mtbsT/modisF_mtbsT,
-         space = as.numeric(gsub('s|t', '', space)),
-         time = as.numeric(gsub('s|t', '', time))) 
+  mutate(
+    # total mtbs events
+    total_mtbs = modisT_mtbsT + modisF_mtbsT,
+    # total modis events
+    total_modis = modisT_mtbsF_modis_over_th + modisT_mtbsT, 
+    # what is the proportion of modis to mtbs fire, regadless of multiple ids
+    modis_to_mtbs_all = modisT_mtbsF_modis_over_th/modisF_mtbsT, 
+    # what is the proportion of modis to mtbs fire, where there are only 1 modis ids
+    modis_to_mtbs_id_1 = num_modisIDs1_and_mtbsT/modisF_mtbsT,
+    # cleaning for plotting
+    space = as.numeric(gsub('s|t', '', space)),
+    time = as.numeric(gsub('s|t', '', time))) 
 
 library(plot3D)
 
