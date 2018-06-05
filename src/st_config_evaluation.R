0fd2d4d0d4042c39c3950b187407e455bdf24396
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-06-05 11:34:17 -0600

    added accuracy assessment

--- src/st_config_evaluation.R
+++ src/st_config_evaluation.R
@@ -1,5 +1,17 @@
 
+##### the fields in st_config
 
+# modisT_mtbsT: number of mtbs fires with at least one modis fire pixel
+# modisF_mtbsT: number of mtbs fires with 0 modis pixels
+# modisT_mtbsF_all_modis: number of total modis IDs MINUS the modisT_mtbsT column   --- first part calculated from the modis rasters only
+# modisT_mtbsF_modis_over_th: number of total modis IDS over the threshold MINUS modisT_mtbsT column ---  first part calculated from modis rasters only
+# mtbs_w_multiple_modis: mtbs fires containing multiple modis IDs
+# st_combo: space time combo
+# total_mtbs: first two columns added
+# total_modis_over_th: modisT_mtbsT + modisT_mtbsF_modis_over_th
+# diff: absolute value of total mtbs -  total_modis
+
+# description of the accuracy assessment http://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/
 
 st_config <- read_csv(file.path(version_dir, 'confusion_matrix', 'confusion_matrices.csv')) %>%
   left_join(., read_csv(file.path(version_dir, 'confusion_matrix', 'additional_variables_confusion_matrix.csv')), by = 'st_combo') %>%
@@ -13,6 +25,10 @@ st_config <- read_csv(file.path(version_dir, 'confusion_matrix', 'confusion_matr
     modis_to_mtbs_all = modisT_mtbsF_modis_over_th/modisF_mtbsT, 
     # what is the proportion of modis to mtbs fire, where there are only 1 modis ids
     modis_to_mtbs_id_1 = num_modisIDs1_and_mtbsT/modisF_mtbsT,
+    misclassification_rate = (modisT_mtbsF_modis_over_th + modisF_mtbsT)/ (modisT_mtbsF_modis_over_th + modisT_mtbsT + modisF_mtbsT),
+    true_positive_rate = modisT_mtbsT / (modisF_mtbsT	+ modisT_mtbsT),
+    precision = modisT_mtbsT / total_modis,
+    prevelance = (modisF_mtbsT + modisT_mtbsT) / (modisT_mtbsF_modis_over_th + modisT_mtbsT + modisF_mtbsT),
     # cleaning for plotting
     space = as.numeric(gsub('s|t', '', space)),
     time = as.numeric(gsub('s|t', '', time))) 
