f58615d1d9cd1e60a94c1820f51e24b8a57cd167
‘admahood’ <admahood@gmail.com>
2018-06-06 17:19:51 +0000

    repeats good

--- src/fixing_confusing matrix.R
+++ src/fixing_confusing matrix.R
@@ -3,7 +3,6 @@ source("src/a_prep_environment.R")
 
 space <- 1:15
 time <- 1:15
-kounter = 1
 
 corz = detectCores()-1
 registerDoParallel(corz)
@@ -11,18 +10,6 @@ registerDoParallel(corz)
 dir.create("data/tables")
 dir.create("data/long_tables")
 system("aws s3 sync s3://earthlab-natem/modis-burned-area/MCD64A1/C6/result_tables data/tables")
-
-big_table <- tibble(st_combo = NA,
-                        mtbs_w_multiple_modis = NA,
-                        modis_w_multiple_mtbs = NA,
-                        mean_n_modis_per_mtbs = NA,
-                        median_n_modis_per_mtbs = NA,
-                        max_n_modis_per_mtbs = NA,
-                        which_max_modis_per_mtbs = NA,
-                        mean_n_mtbs_per_modis = NA,
-                        median_n_mtbs_per_modis = NA,
-                        max_n_mtbs_per_modis = NA,
-                        which_max_mtbs_per_modis = NA)
 
 foreach(SS = space)%dopar%{
   for(TT in time){
@@ -50,11 +37,46 @@ foreach(SS = space)%dopar%{
                   longfile,
                   " s3://earthlab-natem/modis-burned-area/MCD64A1/C6/long_tables/",
                   longfile))
+  }
+}
+
+big_table <- tibble(st_combo = NA,
+                    mtbs_w_multiple_modis = NA,
+                    modis_w_multiple_mtbs = NA,
+                    mean_n_modis_per_mtbs = NA,
+                    median_n_modis_per_mtbs = NA,
+                    max_n_modis_per_mtbs = NA,
+                    which_max_modis_per_mtbs = NA,
+                    mean_n_mtbs_per_modis = NA,
+                    median_n_mtbs_per_modis = NA,
+                    max_n_mtbs_per_modis = NA,
+                    which_max_mtbs_per_modis = NA,
+                    row_check = NA)
+
+
+system("aws s3 sync s3://earthlab-natem/modis-burned-area/MCD64A1/C6/long_tables data/long_tables")
+kounter = 1
+for(SS in space){
+  for(TT in time){
+    longfile = paste0("long_s",SS,"t",TT,".csv")
+    
+    long_mt_mo <- read.csv(paste0("data/long_tables/", longfile)) %>% as_tibble()
+    
+    ## rowcheck
+    results <- read.csv(paste0("data/tables/mtbs_modis_ids_ba_s",SS,"t",TT,".csv"),
+                        stringsAsFactors = F) %>% as_tibble()
+    rc = table(results$n) %>% as_tibble()
+    NN <- sum(rc$n[1:2])
+    rc <- rc[3:nrow(rc),]
+    for(ROW in 1:nrow(rc)){
+      XX <- as.numeric(rc$Var1[ROW])
+      NN <- NN + (XX * rc$n[ROW])
+    }
     
     n_modis_per_mtbs <- table(long_mt_mo$Fire_ID) %>% 
       as_tibble() %>%
       filter(Var1 != "NA")
-    
+      
     n_mtbs_per_modis <- table(long_mt_mo$modis_id) %>% 
       as_tibble() %>%
       filter(Var1 != "NA")
@@ -70,15 +92,14 @@ foreach(SS = space)%dopar%{
     big_table[kounter,9] <- median(n_mtbs_per_modis$n)
     big_table[kounter,10] <- max(n_mtbs_per_modis$n)
     big_table[kounter,11] <- n_mtbs_per_modis[max(n_modis_per_mtbs$n),]$Var1
-    
+    big_table[kounter,12] <- NN==nrow(long_mt_mo)
 
     kounter = kounter +1
     print(c(SS,TT))
-    print(Sys.time())
-    print(Sys.time() - t0)
+    print(nrow(long_mt_mo)==NN)
+
     
-  }
-}
+}}
 
 write.csv(big_table, "data/repeats.csv")
 system("aws s3 cp data/repeats.csv s3://earthlab-natem/modis-burned-area/MCD64A1/C6/confusion_matrix/repeats.csv")
