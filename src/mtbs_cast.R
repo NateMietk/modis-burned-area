1c06e927289df4300ad7023d8f5ff80f860dd573
‘admahood’ <admahood@gmail.com>
2018-06-09 14:27:46 +0000

    splitting mtbs polys for parallelization

--- src/mtbs_cast.R
+++ src/mtbs_cast.R
@@ -1,5 +1,11 @@
+# a working script to figure out how to split up polygons for the confusion matrix par script
+
 mtbs_cast <- st_cast(mtbs, to = "POLYGON")
 
 mtbs_cast$duped <- duplicated(mtbs_cast$Fire_ID)
 
-x = table(mtbs_cast$Fire_ID) %>% as_tibble()
+mtbs_cast$new_id <- ifelse(mtbs_cast$duped == TRUE,
+                           paste(as.character(mtbs_cast$Fire_ID),as.character(row_number(mtbs_cast$Fire_ID)), sep="_"),
+                           as.character(mtbs_cast$Fire_ID))
+
+mtbs_cast$cast_area_ha <- st_area(mtbs_cast[0])%>% set_units(value = hectare)
