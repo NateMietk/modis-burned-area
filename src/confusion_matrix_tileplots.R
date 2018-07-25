e74be0986ec5d37a132657ac10c8c3bf7d34a164
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-07-24 21:21:19 -0600

    rename and update - general

--- src/confusion_matrix_tileplots.R
+++ src/confusion_matrix_tileplots.R
@@ -3,30 +3,6 @@ library(ggplot2)
 library(viridis)
 library(scales)
 
-x <- read.csv("data/confusion_matrices.csv", stringsAsFactors = FALSE)
-
-x$sum_means <- x$mean_n_modis_per_mtbs + x$mean_n_mtbs_per_modis
-x$ismin <- x$sum_means == min(x$sum_means)
-
-ggplot(x, aes(x = space, y = time)) +
-  scale_color_manual(values = c(NA, "black")) +
-  geom_tile(aes(fill = sum_means, color=ismin), lwd=1) +
-  scale_fill_viridis(direction = -1, values=rescale(c(1,1.1,5))) +
-  theme_bw()
-
-ggplot(x, aes(x = space, y = time)) +
-  scale_color_manual(values = c(NA, "black")) +
-  geom_tile(aes(fill = mean_n_modis_per_mtbs, color=ismin), lwd=1) +
-  scale_fill_viridis(direction = -1, values=rescale(c(1,1.1,5))) +
-  theme_bw()
-
-ggplot(x, aes(x = space, y = time)) +
-  scale_color_manual(values = c(NA, "black")) +
-  geom_tile(aes(fill = mean_n_mtbs_per_modis, color=ismin), lwd=1) +
-  scale_fill_viridis(direction = -1) +
-  theme_bw()
-
-
 confusing <- read_csv(file.path(version_dir, 'confusion_matrix', 'confusion_matrices_casted.csv')) %>%
   mutate(ratio = modisT_mtbsT/mtbsT_modisT_unique_modis_events)
 
