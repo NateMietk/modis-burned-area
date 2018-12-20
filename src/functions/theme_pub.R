be1e299163a64c1312784d25bc992afc0f697a1b
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-12-19 23:13:06 -0700

    major file cleaning

---
+++ src/functions/theme_pub.R
@@ -0,0 +1,28 @@
+theme_pub <- function(base_size=11, base_family="") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(hjust = 0.05, size = 13),
+            
+            panel.border = element_rect(colour = NA),
+            panel.background = element_rect(colour = NA),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            plot.background = element_rect(colour = NA),
+            
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            
+            legend.title = element_text(size=11),
+            legend.position = "right",
+            legend.text = element_text(size=11),
+            legend.direction = "vertical",
+            legend.key = element_rect(colour = "transparent", fill = "white"),
+            
+            strip.background=element_rect(colour=NA),
+            strip.text.x = element_text(size = 10),
+            
+            axis.title = element_text(size = 11),
+            axis.text.x = element_text(size = 10, angle = 65, hjust = 1),
+            axis.text.y = element_text(size = 11)))
+}
