a7d552d536214499512f7de286824c1d3d4ba22f
‘admahood’ <admahood@gmail.com>
2018-12-26 21:12:04 -0700

    tabulated intrayear reburn stats

--- src/r/xxx_calculate_reburn_stats_for_paper.R
+++ src/r/xxx_calculate_reburn_stats_for_paper.R
@@ -1,10 +1,17 @@
 library(tidyverse)
 
 irbs<-read_csv("tables/intrayear_reburns.csv") %>%
-  select(-X1) %>%
   mutate(tile = substr(observation,10,15),
          year = substr(observation,16,20)) %>%
   group_by(tile) %>%
-  summarise(mean_reburn_pct=mean(reburn_pct),sd_reburn_pct = sd(reburn_pct))
+  summarise(mean_reburn_pct=mean(reburn_pct),sd_reburn_pct = sd(reburn_pct),
+            mean_n2 = mean(n2_or_more), sum(n2_or_more))
 
 write_csv(irbs, "tables/reburn_stats.csv")
+
+
+all_but_florida <- read_csv("tables/intrayear_reburns.csv") %>%
+  mutate(tile = substr(observation,10,15)) %>%
+  filter(tile != "h10v06") %>%
+  summarise(mean_reburn_pct=mean(reburn_pct),sd_reburn_pct = sd(reburn_pct),
+            mean_n2 = mean(n2_or_more), sum(n2_or_more))
