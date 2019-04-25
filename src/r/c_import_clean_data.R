8094fedc7fbcb3432fa0aa6fdf55fbf1c347a3ec
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-04-25 14:31:36 -0600

    add 50k fishnet; add fired shapfile; reworked the ecoregion file names

--- src/r/c_import_clean_data.R
+++ src/r/c_import_clean_data.R
@@ -1,17 +1,17 @@
 # Download and import the Level 4 Ecoregions data, which has Levels 4, 3, 2, 1
 # Download will only happen once as long as the file exists
 if (!exists("ecoregions_l4321")){
-  if(!file.exists(file.path(ecoregionl4_prefix, 'us_eco_l4_no_st.shp'))) {
+  if(!file.exists(file.path(raw_dir_ecoregionl4, 'us_eco_l4_no_st.shp'))) {
     # Download ecoregion level 4
-    ecol4_shp <- file.path(ecoregionl4_prefix, 'us_eco_l4_no_st.shp')
+    ecol4_shp <- file.path(raw_dir_ecoregionl4, 'us_eco_l4_no_st.shp')
     download_data("ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/us_eco_l4.zip",
-                  ecoregionl4_prefix,
+                  raw_dir_ecoregionl4,
                   ecol4_shp,
                   'us_eco_l4_no_st',
-                  ecoregionl4_prefix)
+                  raw_dir_ecoregionl4)
     }
   
-  ecoregions_l4321 <- st_read(file.path(ecoregionl4_prefix, 'us_eco_l4_no_st.shp')) %>%
+  ecoregions_l4321 <- st_read(file.path(raw_dir_ecoregionl4, 'us_eco_l4_no_st.shp')) %>%
     sf::st_transform(st_crs(usa)) %>%
     st_make_valid() %>%
     group_by(US_L4NAME, US_L3NAME, NA_L2NAME, NA_L1NAME) %>%
@@ -29,32 +29,46 @@ if (!exists("ecoregions_l4321")){
     setNames(tolower(names(.)))
   
   ecoregions_l4321 %>%
-    st_write(., file.path(ecoregion_out, 'us_eco_l4321.gpkg'),
+    st_write(., file.path(ecoregion_dir, 'us_eco_l4321.gpkg'),
              driver = 'GPKG', delete_layer = TRUE)
 } else {
-  ecoregions_l4321 <- st_read(file.path(ecoregion_out, 'us_eco_l4321.gpkg'))
+  ecoregions_l4321 <- st_read(file.path(ecoregion_dir, 'us_eco_l4321.gpkg'))
   }
+
+# 50k Fishnet
+if (!exists("fishnet_50k")) {
+  if (!file.exists(file.path(fishnet_dir, "fishnet_50k.gpkg"))) {
+    fishnet_50k <- sf::st_make_grid(states, cellsize = 50000, what = 'polygons') %>%
+      sf::st_sf('geometry' = ., data.frame('fishid50k' = 1:length(.))) %>%
+      sf::st_intersection(., st_union(states))
+    
+    sf::st_write(fishnet_50k,
+                 file.path(fishnet_dir, "fishnet_50k.gpkg"),
+                 driver = "GPKG")
+    
+  } else {
+    fishnet_50k <- sf::st_read(file.path(fishnet_dir, "fishnet_50k.gpkg"))
+  }
+}
 
 # Import and clean the MTBS polygons
 if (!exists('mtbs_fire')) {
   
-  mtbs_shp <- file.path(mtbs_prefix, 'mtbs_perimeter_data_v2','dissolve_mtbs_perims_1984-2015_DD_20170501.shp')
+  mtbs_shp <- file.path(raw_dir_mtbs, 'mtbs_perimeter_data')
   if (!file.exists(mtbs_shp)) {
-    file.download(file.path(mtbs_prefix, 'mtbs_perimeter_data_v2','dissolve_mtbs_perims_1984-2015_DD_20170501.shp'),
-                  mtbs_prefix, "https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/MTBS_Fire/data/composite_data/burned_area_extent_shapefile/mtbs_perimeter_data.zip")
-    
-  }
+    file_download(file.path(raw_dir_mtbs, 'mtbs_perimeter_data','mtbs_perims_DD.shp'),
+                  raw_dir_mtbs, "https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/MTBS_Fire/data/composite_data/burned_area_extent_shapefile/mtbs_perimeter_data.zip")}
   
-  mtbs_fire <- st_read(dsn = file.path(mtbs_prefix, 'mtbs_perimeter_data_v2'),
-                       layer = 'dissolve_mtbs_perims_1984-2015_DD_20170501', quiet= TRUE) %>%
+  mtbs_fire <- st_read(dsn = file.path(raw_dir_mtbs, 'mtbs_perimeter_data'),
+                       layer = 'mtbs_perims_DD', quiet= TRUE) %>%
     filter(Year >= '2001') %>%
-    st_transform(p4string_ea) %>%
+    st_transform(st_crs(states)) %>%
     mutate(discovery_date = ymd(paste(Year, StartMonth, StartDay, sep="-")),
            discovery_year = year(discovery_date),
            discovery_day = day(discovery_date),
            discovery_month = month(discovery_date),
            discovery_doy = yday(discovery_date)) %>%
-    st_intersection(., st_union(usa)) %>%
+    st_intersection(., st_union(states)) %>%
     rename_all(tolower) %>%
     dplyr::select(fire_id, fire_name, discovery_date, discovery_year, discovery_day, discovery_month, discovery_doy, acres) %>%
     # Below we are categorizing the fires as in the East or West based on the -97th parallel - which is what MTBS uses
@@ -62,22 +76,21 @@ if (!exists('mtbs_fire')) {
     sfc_as_cols(., st_centroid(geometry)) %>%
     mutate(mtbs_region = ifelse(x < -97, 'West', 'East')) %>%
     dplyr::select(-x, -y) %>%
-    st_transform(p4string_ea)
+    st_transform(st_crs(states))
 }
 
-if(!file.exists(file.path(fire_dir, 'mtbs_ecoreg.gpkg'))) {
-  mtbs_ecoreg <- mtbs_fire %>%
+if(!file.exists(file.path(mtbs_dir, 'lvl4321_eco_mtbs.gpkg'))) {
+  mtbs_ecoregion <- mtbs_fire %>%
     st_intersection(., ecoregions_l4321) %>%
-    mutate(mtbs_ba_ha = acres*0.404686,
-           mtbs_ba_ecoreg_ha = as.numeric(st_area(geometry))*0.0001) 
+    mutate(mtbs_burn_area = as.numeric(st_area(.))/1000000) 
   
-  mtbs_ecoreg %>%
-    st_write(., file.path(fire_dir, 'mtbs_ecoreg.gpkg'), delete_layer = TRUE)
+  mtbs_ecoregion %>%
+    st_write(., file.path(mtbs_dir, 'lvl4321_eco_mtbs.gpkg'), delete_layer = TRUE)
   
   system(paste0('aws s3 sync data' , ' ', s3_base))
   
 } else {
-  mtbs_ecoreg <- st_read(file.path(fire_dir, 'mtbs_ecoreg.gpkg'))
+  mtbs_ecoregion <- st_read(file.path(mtbs_dir, 'lvl4321_eco_mtbs.gpkg'))
 }
 
 # Import Landfire to class out agriculture
@@ -108,4 +121,74 @@ if(!file.exists(file.path(evt_dir, 'us_agriculture_1km.tif'))) {
   ag_rst_1km <- raster::raster(file.path(evt_dir, 'us_agriculture_1km.tif'))
   }
   
+# Import and polygonize the modis event data
+if(!file.exists(file.path(fired_dir, 'usa_fired_2001_2017.gpkg'))) {
+  yr_list <- rep(2001:2017)
   
+  library(foreach)
+  library(doParallel)
+  
+  cl <- parallel::makeCluster(parallel::detectCores()/2)
+  doParallel::registerDoParallel(cl)
+  
+  full_poly <- foreach(y = unique(yr_list), .combine = rbind, .packages = c('foreach')) %dopar% {
+    rst_list <- list.files(yearly_events_dir, pattern = paste0(y), full.names = TRUE)
+    
+    full_df <- foreach(x = rst_list, .combine = cbind, .packages = c('foreach', 'tidyverse', 'raster', 'sf', 'velox', 'stars')) %do% {
+      
+      file_basename <- x %>%
+        basename %>%
+        strsplit(split = "\\.") %>%
+        unlist
+      
+      file_split <- file_basename[1] %>%
+        strsplit(split = "_") %>%
+        unlist
+      
+      yr <- file_split[3]
+      var <- file_split[4]
+      
+      rst <- raster(x) 
+      rst[rst == 0] <- NA
+      
+      poly_out <- rst %>%
+        st_as_stars() %>%
+        st_as_sf() %>%
+        mutate(year = yr) 
+      
+      if(var == 'events') {
+        poly_out <- poly_out %>%
+          mutate(events = paste0(layer, '_', year)) %>%
+          dplyr::select(events, year)
+      } else if('layer' %in% colnames(poly_out)) {
+        poly_out <- poly_out %>%
+          dplyr::select(!!ensym(var) := layer, year)
+      } else {
+        poly_out <- poly_out %>%
+          dplyr::select(!!ensym(var) := file_basename[1], year)
+      }
+      return(poly_out)
+    }
+    return(full_df)
+  }
+  parallel::stopCluster(cl)
+  
+  usa_fired <- full_poly %>%
+    dplyr::select(-contains('.')) %>%
+    group_by(events) %>%
+    summarise_all(mode)
+  st_write(usa_fired, file.path(fired_dir, 'usa_fired_2001_2017.gpkg'))
+} else {
+  usa_fired <- st_read(file.path(fired_dir, 'usa_fired_2001_2017.gpkg'))
+}
+  
+# Spatially join ecoregions to the FIRED 
+if (!file.exists(file.path(fired_dir, 'lvl4321_eco_fired.gpkg'))) {
+  fired_ecoregion <- usa_fired %>%
+    st_intersection(., ecoregions_l4321) %>%
+    mutate(fired_burn_area = as.numeric(st_area(.))/1000000) 
+  
+  st_write(fired_ecoregion, file.path(fired_dir, 'lvl4321_eco_fired.gpkg'), delete_layer = TRUE)
+} else {
+  fired_ecoregion <- st_read(file.path(fired_dir, 'lvl4321_eco_fired.gpkg'))
+}
