50b5656d81a1377bb5b77f243eefda068987b001
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-04-25 14:32:01 -0600

    overhaul on directory names - much more consistent now

--- src/r/a_prep_environment.R
+++ src/r/a_prep_environment.R
@@ -7,7 +7,7 @@
 #   '--with-proj-lib=/Library/Frameworks/PROJ.framework/unix/lib'))
 
 packages <- c("tidyverse", "magrittr", "raster", "RCurl", "gdalUtils", "foreach", "doParallel", "sf", 
-       "assertthat", 'lubridate', 'viridis', 'scales', 'velox', 'ggmap', 'Hmisc', 'devtools',
+       "assertthat", 'lubridate', 'viridis', 'scales', 'velox', 'ggmap', 'Hmisc', 'devtools', 'rvest',
        'classInt', 'RColorBrewer', 'rasterVis', 'RStoolbox', 'gridExtra', 'ggthemes', 'tidyr', 'broom')
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
@@ -19,89 +19,63 @@ if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
 # devtools::install_github("NateMietk/FIREDr", force = TRUE)
 # library(FIREDr)
 
+# load all functions
+file_sources <- list.files(file.path('src', 'functions'), pattern="*.R", 
+                           full.names=TRUE, ignore.case=TRUE)
+invisible(sapply(file_sources, source, .GlobalEnv))
+
 p4string_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
 
 # Raw data folders
-prefix <- "data"
-raw_prefix <- file.path(prefix, "raw")
-ag_raw_dir <- file.path(raw_prefix, "US_140EVT_20180618")
-us_prefix <- file.path(raw_prefix, "cb_2016_us_state_20m")
-mtbs_prefix <- file.path(raw_prefix, "mtbs_fod_perimeter_data")
-ecoregion_prefix <- file.path(raw_prefix, "ecoregions")
-ecoregionl4_prefix <- file.path(raw_prefix, "us_eco_l4")
+data_dir <- "data"
+raw_dir <- file.path(data_dir, "raw")
+raw_dir_ag <- file.path(raw_dir, "US_140EVT_20180618")
+raw_dir_us <- file.path(raw_dir, "cb_2016_us_state_20m")
+raw_dir_mtbs <- file.path(raw_dir, "mtbs")
+raw_dir_ecoregion <- file.path(raw_dir, "ecoregions")
+raw_dir_ecoregionl4 <- file.path(raw_dir, "us_eco_l4")
 
-# Output folders
-MCD64A1_dir <- file.path(prefix, "MCD64A1")
-version_dir <- file.path(MCD64A1_dir, "C6")
-hdf_months <- file.path(version_dir, "hdf_months")
-tif_months <- file.path(version_dir, "tif_months")
-yearly_mosaic <- file.path(version_dir, "yearly_mosaic")
-monthly_mosaic <- file.path(version_dir, "monthly_mosaic")
+# Check if directory exists for all variable aggregate outputs, if not then create
+var_dir <- list(data_dir, raw_dir, raw_dir_ag, raw_dir_us, raw_dir_mtbs, raw_dir_ecoregion, raw_dir_ecoregionl4)
+lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
 
-yearly_events <- file.path(version_dir, "yearly_events")
-stat_out <- file.path(version_dir, 'lvl1_eco_stats')
+# MODIS related folders
+MCD64A1_dir <- file.path(data_dir, "MCD64A1")
+C6_dir <- file.path(MCD64A1_dir, "C6")
+hdf_months_dir <- file.path(C6_dir, "hdf_months")
+tif_months_dir <- file.path(C6_dir, "tif_months")
+yearly_mosaic_dir <- file.path(C6_dir, "yearly_mosaic")
+monthly_mosaic_dir <- file.path(C6_dir, "monthly_mosaic")
+yearly_events_dir <- file.path(C6_dir, "yearly_events")
+yearly_stats_dir <- file.path(C6_dir, "yearly_events")
 
-bounds_crt <- file.path(prefix, "bounds")
-evt_dir <- file.path(bounds_crt, 'us_140evt')
-ecoreg_crt <- file.path(bounds_crt, "ecoregions")
-ecoregion_out <- file.path(ecoreg_crt, "us_eco_l3")
+# Check if directory exists for all variable aggregate outputs, if not then create
+var_dir <- list(MCD64A1_dir, C6_dir, hdf_months_dir, tif_months_dir, yearly_mosaic_dir, 
+                monthly_mosaic_dir, yearly_events_dir, yearly_stats_dir)
+lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
+
+# Data output folders
+bounds_dir <- file.path(data_dir, "bounds")
+evt_dir <- file.path(bounds_dir, 'us_140evt')
+ecoregion_dir <- file.path(bounds_dir, "ecoregions")
+ecoregionl3_dir <- file.path(ecoreg_crt, "us_eco_l3")
 fire_dir <- file.path(prefix, 'fire')
+mtbs_dir <- file.path(fire_dir, 'mtbs')
+fired_dir <- file.path(fire_dir, 'fired')
+fishnet_dir <- file.path(bounds_dir, 'fishnet')
+
+# Check if directory exists for all variable aggregate outputs, if not then create
+var_dir <- list(bounds_dir, evt_dir, ecoregion_dir, ecoregionl3_dir, fire_dir, fishnet_dir, mtbs_dir, fired_dir)
+lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
 
 s3_base <- 's3://earthlab-natem/modis-burned-area'
 
-# Check if directory exists for all variable aggregate outputs, if not then create
-var_dir <- list(prefix, raw_prefix, us_prefix, MCD64A1_dir, version_dir, mtbs_prefix, ag_raw_dir, evt_dir,
-                hdf_months, tif_months, yearly_mosaic, monthly_mosaic, ecoregion_prefix,
-                ecoregionl4_prefix, bounds_crt, ecoreg_crt, ecoregion_out, fire_dir, stat_out)
+# Figure and Table directories
+results_dir <- 'results'
+draft_figs_dir <- file.path(results_dir, 'draft_figures')
+draft_table_dir <- file.path(results_dir, 'draft_tables')
+
+var_dir <- list(results_dir, draft_figs_dir, draft_table_dir)
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
 
-# Function to download files
-file.download <- function(shp_path_name, shp_dir, url){
-  if (!file.exists(shp_path_name)) {
-    dest <- paste0(shp_dir, ".zip")
-    download.file(url, dest)
-    unzip(dest, exdir = shp_dir)
-    unlink(dest)
-    assert_that(file.exists(shp_path_name))
-  }
-}
-
-theme_pub <- function(base_size=13, base_family="") {
-  library(grid)
-  library(ggthemes)
-  (theme_foundation(base_size=base_size, base_family=base_family)
-    + theme(plot.title = element_text(hjust = 0.05, size = 13),
-            
-            panel.border = element_rect(colour = NA),
-            panel.background = element_rect(colour = NA),
-            panel.grid.major = element_line(colour="#f0f0f0"),
-            panel.grid.minor = element_blank(),
-            plot.background = element_rect(colour = NA),
-            
-            axis.line = element_line(colour="black"),
-            axis.ticks = element_line(),
-            
-            legend.title = element_text(size=14),
-            legend.text = element_text(size=13),
-            legend.direction = "vertical",
-            legend.key = element_rect(colour = "transparent", fill = "transparent"),
-            
-            strip.background=element_rect(colour=NA),
-            strip.text.x = element_text(size = 10),
-            
-            axis.title = element_text(size = 16),
-            axis.text.x = element_text(size = 16),
-            axis.text.y = element_text(size = 16)))
-}
-
-download_data <-  function(url, dir, layer, rp = raw_prefix) {
-  dest <- paste0(rp, ".zip")
-  
-  if (!file.exists(layer)) {
-    download.file(url, dest)
-    unzip(dest, exdir = dir)
-    unlink(dest)
-    assert_that(file.exists(file.path(dir, paste0(layer, '.shp'))))
-  }
-}
 
