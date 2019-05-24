9099864db5e012469cbae78420af6e949fecdd93
‘admahood’ <admahood@gmail.com>
2019-05-24 16:03:28 +0000

    event delineation streamlined for ec2

--- src/r/event_classification_using_dataframe.R
+++ src/r/event_classification_using_dataframe.R
@@ -1,10 +1,18 @@
-# aspatial event creation
+# "aspatial" event creation
+# author: Adam Mahood, May 2019
+
 libs <- c("tidyverse", "raster", "foreach", "doParallel","sf")
 lapply(libs, library, character.only = TRUE, verbose = FALSE)
+
 dir.create("data")
 dir.create("data/scrap")
 dir.create("data/scrap/tif_alltiles")
-system("aws s3 sync s3://earthlab-natem/modis-burned-area/MCD64A1/C6/tif_converted_alltiles/ /home/a/projects/modis-burned-area/data/scrap/tif_alltiles/")
+
+wd <- getwd()
+
+system(paste("aws s3 sync",
+             "s3://earthlab-natem/modis-burned-area/MCD64A1/C6/tif_converted_alltiles/"
+             ,file.path(wd, "data/scrap/tif_alltiles/")))
 
 tifs <- list.files("data/scrap/tif_alltiles", full.names = T) 
 
@@ -29,75 +37,13 @@ print(Sys.time()-t0) # 15 minutes on adam's computer
 t1 <- Sys.time()
 
 write_csv(dd, "data/bd_no_events.csv")
-
-dd$event <- NA
-# xrange <- unique(dd$x) %>% sort
-# yrange <- unique(dd$y) %>% sort
-#zrange <- unique(dd$burn_date) %>% sort
+system("aws s3 cp data/bd_no_events.csv s3://earthlab-natem/modis-burned-area/MCD64A1/C6/bd_no_events.csv")
 
 ss <- 5
 tt <- 11
 new_id <- 1
-
-# slow way ------------------------------
-# t0 <- Sys.time()
-# for (zz in zrange){
-#   print(zz)
-#   xrange<-unique(dd[dd$burn_date < zz + 30 & dd$burn_date > zz - 30,])$x
-#    
-#  #xrange <- seq(min(xr), max(xr))
-#   
-#   for (xx in xrange){
-#     yrange<-unique(dd[dd$burn_date < zz + 30 & dd$burn_date > zz - 30 & dd$x < xx+30 & dd$x > xx-30,])$y
-#     #yrange <- seq(min(yr), max(yr))
-#     #print(xx)
-#     for (yy in yrange){
-#     
-#       st_cube_x <- seq(xx, xx + ss*463, by = 463)
-#       st_cube_y <- seq(yy, yy + ss*463, by = 463)
-#       st_cube_z <- seq(zz, zz + tt)
-#       
-#       data_slice <- dd %>%
-#         filter(burn_date > min(st_cube_z) - 1 & burn_date < max(st_cube_z) + 1 &
-#         x > min(st_cube_x) - 1 & x < max(st_cube_x) + 1 &
-#         y > min(st_cube_y) - 1 & y < max(st_cube_y) + 1)
-#       
-#       
-#       if(nrow(data_slice)>0){ print(nrow(data_slice))}
-#       
-#       if(nrow(data_slice) > 0) {
-#         if(length(unique(data_slice$event)) == 1){
-#           if(is.na(unique(data_slice$event))){
-#             dd[dd$x %in% data_slice$x & 
-#                  dd$y %in% data_slice$y  &
-#                  dd$burn_date %in% data_slice$burn_date,]$event <- new_id
-#             new_id <- new_id+1
-#           }else{
-#             dd[dd$x %in% data_slice$x & 
-#                  dd$y %in% data_slice$y  &
-#                  dd$burn_date %in% data_slice$burn_date,]$event <- unique(data_slice$event)
-#             
-#           } # end of else (when the the one value is an event)
-#         }else{
-#           lowest_id <- unique(data_slice$event) %>%
-#             na.omit() %>%
-#             min()
-#           dd[dd$x %in% data_slice$x & 
-#                dd$y %in% data_slice$y  &
-#                dd$burn_date %in% data_slice$burn_date,]$event <- lowest_id
-#           
-#         } # end of else (when rows are > 1)
-#       } # end of if(data_slice>0)
-#     }# end of xyz for loops
-#   }
-# }
-# print(Sys.time()-t0)
-# write_csv(dd, "data/r_events.csv")
-# system("aws s3 cp /home/a/projects/modis-burned-area/data/r_events.csv s3://earthlab-natem/modis-burned-area/MCD64A1/C6/r_events.csv")
-
-# better way -----------------------------------------
-new_id <- 1
 dd$event <- NA
+
 t0 <- Sys.time()
 for(i in 1:nrow(dd)){
   if(is.na(dd$event[i])){
