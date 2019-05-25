ce9b8d28e3d94a5df6b2a33d99a4a370e3216eed
‘admahood’ <admahood@gmail.com>
2019-05-25 18:31:01 +0000

    l

--- src/r/event_classification_using_dataframe.R
+++ src/r/event_classification_using_dataframe.R
@@ -1,8 +1,16 @@
 # "aspatial" event creation
 # author: Adam Mahood, May 2019
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
 
-libs <- c("tidyverse", "raster", "foreach", "doParallel","sf")
+## install funique from github
+remotes::install_github("mkearney/funique")
+
+libs <- c("tidyverse", "raster", "foreach", "doParallel","sf","funique")
 lapply(libs, library, character.only = TRUE, verbose = FALSE)
+
+
 
 dir.create("data")
 dir.create("data/scrap")
@@ -46,61 +54,54 @@ dd <- read_csv("data/bd_no_events.csv")
 
 resolution<- 463.3127
 
-ss <- 5/2*resolution
-tt <- 11/2 * resolution
+sspace<-5
+ttime<-11
+
+ss <- sspace/2 * resolution
+tt <- ttime/2 * resolution
 new_id <- 1
-dd$event <- NA
 
-# condition1 <- nrow(data_slice) > 0
-# condition2 <-length(unique(data_slice$event)) == 1
-# condition3 <- is.na(unique(data_slice$event))
-
+ddrows <- nrow(dd)
+dd$rownums = 1:nrow(dd)
 t0 <- Sys.time()
-for(i in 1:nrow(dd)){
-  if(is.na(dd$event[i])){
-    print(paste(i, round(i/nrow(dd)*100, 3), "%"))
-    st_cube_x <- c(dd$x[i] - ss, dd$x[i] + ss)
-    st_cube_y <- c(dd$y[i] - ss, dd$y[i] + ss)
-    st_cube_z <- c(dd$burn_date[i]-tt, dd$burn_date[i] + tt)
+vector_out <- rep(NA, ddrows)
+
+for(i in 1:ddrows){
+  if(is.na(vector_out[i])){
+    print(paste(i, round(i/ddrows*100, 3), "%"))
+    st_cube<- c(dd$x[i] - ss, dd$x[i] + ss,
+                dd$y[i] - ss, dd$y[i] + ss,
+                dd$burn_date[i]-tt, dd$burn_date[i] + tt)
     
-    data_slice <- dd %>%
-      filter(burn_date > st_cube_z[1] - 1 & burn_date < st_cube_z[2] + 1 &
-               x > st_cube_x[1] - 1 & x < st_cube_x[2] + 1 &
-               y > st_cube_y[1] - 1 & y < st_cube_y[2] + 1)
+    rs<- filter(dd,burn_date >= st_cube[5] & 
+                  burn_date <= st_cube[6]  &
+                  x >= st_cube[1] & x <= st_cube[2] &
+                  y >= st_cube[3] & y <= st_cube[4])$rownums
+    ude <- funique(vector_out[rs])
     
-    if(nrow(data_slice) > 0) {
-      if(length(unique(data_slice$event)) == 1){
-        if(is.na(unique(data_slice$event))){
-          dd[dd$x %in% data_slice$x & 
-               dd$y %in% data_slice$y  &
-               dd$burn_date %in% data_slice$burn_date,]$event <- new_id
-          new_id <- new_id+1
-        }else{
-          dd[dd$x %in% data_slice$x & 
-               dd$y %in% data_slice$y  &
-               dd$burn_date %in% data_slice$burn_date,]$event <- unique(data_slice$event)
-          
-        } # end of else (when the the one value is an event)
-      }else{
-        lowest_id <- unique(data_slice$event) %>%
-          na.omit() %>%
-          min()
-        dd[dd$x %in% data_slice$x & 
-             dd$y %in% data_slice$y  &
-             dd$burn_date %in% data_slice$burn_date,]$event <- lowest_id
-        
-      } # end of else (when rows are > 1)
-    } # end of if(data_slice>0)
-   
+    if(length(ude) == 1){
+      if(is.na(ude)){
+        vector_out[rs] <- new_id
+        new_id <- new_id+1
+      }
+    }else{
+      vector_out[rs] <- ude %>%
+        na.omit() %>%
+        min()
+    } 
   }
 }
+
+dd$event <- vector_out
 print(Sys.time()-t0)
 write_csv(dd, "data/r_events.csv")
-system("aws s3 cp /home/a/projects/modis-burned-area/data/r_events.csv s3://earthlab-natem/modis-burned-area/MCD64A1/C6/r_events_s5d_t11d.csv")
+system(paste0("aws s3 cp data/r_events.csv ",
+              "s3://earthlab-natem/modis-burned-area/MCD64A1/C6/event_dataframes/r_events_s",
+              sspace, "_t" ,ttime, ".csv"))
 
-
-crss <- crs(raster(tifs[1]), asText=T)
-subset <- filter(dd, burn_date<550) %>%
-  st_as_sf(coords = c("x", "y"),crs = crss)
-  
-st_write(subset, "subset.gpkg", delete_dsn = TRUE)
+# 
+# crss <- crs(raster(tifs[1]), asText=T)
+# subset <- filter(dd, burn_date<550) %>%
+#   st_as_sf(coords = c("x", "y"),crs = crss)
+#   
+# st_write(subset, "subset.gpkg", delete_dsn = TRUE)
