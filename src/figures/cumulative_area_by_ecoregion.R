b6a054282c57bbd5f8a7676f82d049e0718776ac
‘admahood’ <admahood@gmail.com>
2019-06-12 14:12:16 -0500

    cumulative area figure script has been cleaned up and commented

--- src/figures/cumulative_area_by_ecoregion.R
+++ src/figures/cumulative_area_by_ecoregion.R
@@ -1,86 +1,19 @@
+libs <- c("tidyverse", "sf")
+lapply(libs, library, character.only = TRUE)
 
-# getting landcover
-ll<-list()
-cc <- 1
-years <- 2002:2018
-for(y in years){
-  lc <- raster(file.path(local_data, paste0("MCD12Q1_mosaics/usa_lc_mosaic_",y-1,".tif")))
-  ll[[cc]] <- df[df$year == y,] %>%
-    st_as_sf(coords = c("x","y"), crs = crs(template, asText=TRUE))%>%
-    mutate(lc = raster::extract(x=lc, y=.))
-  cc<-cc+1
-}
+daily_file <- Sys.glob("data/daily_stats*")
+ecoregion_file <- "/home/a/data/background/ecoregions/us_eco_l1.gpkg"
+daily<- read_csv(daily_file)
 
-df_lc <- do.call("rbind", ll) %>%
-  mutate(l1_eco = raster::extract(x=e_rast, y=.))
-
-st_write(df_lc, "data/modis_events_w_landcover_01_18.gpkg")
-
-
-labels <- st_set_geometry(ecoregions,NULL) %>%
-  mutate(dup = duplicated(NA_L1CODE)) %>%
-  filter(dup == FALSE) %>%
-  dplyr::select(-dup) %>%
-  dplyr::rename(l1_eco = NA_L1CODE)
-
-lc_labels <- read_csv("data/usa_landcover_t1_classes.csv") %>%
-  rename(lc = value, lc_name = name)
-
-daily <- df_lc %>%
-  st_set_geometry(NULL) %>%
-  group_by(id, date) %>%
-  summarise(pixels = n(),
-            l1_eco = getmode(l1_eco),
-            lc = getmode(lc)) %>%
-  ungroup() %>%
-  group_by(id) %>%
-  mutate(cum_pixels = cumsum(pixels),
-         total_pixels = sum(pixels),
-         ignition_date = min(date),
-         last_date = max(date),
-         duration = as.numeric(last_date-ignition_date)+1,
-         simple_fsr_pixels = total_pixels/as.numeric(duration), 
-         simple_fsr_km2 = total_pixels/as.numeric(duration)* 463*463/1000000)%>%
-  ungroup()%>%
-  mutate(daily_area_km2 = pixels * 463*463/1000000,
-         cum_area_km2 = cum_pixels * 463*463/1000000,
-         total_area_km2 = total_pixels * 463*463/1000000,
-         pct_total_area = pixels/total_pixels*100,
-         pct_cum_area = pixels/cum_pixels*100,
-         event_day = as.numeric(date - ignition_date + 1),
-         ratio_area_added_to_average = daily_area_km2/simple_fsr_km2,
-         prior_pixels = cum_pixels-pixels,
-         rel_fsr_per_day = ifelse(prior_pixels>0,pixels/prior_pixels, 0) # need to decide on a value for that
-  ) %>%
-  left_join(labels) %>%
-  left_join(lc_labels) %>%
-  filter(is.na(l1_eco) == F)
-
-write_csv(daily,"data/daily_06_10.csv")
-
-event <- daily %>%
-  group_by(id) %>%
-  summarise(lc_name = getmode(lc_name),
-            l1_ecoregion = getmode(l1_ecoregion),
-            ignition_date = first(ignition_date),
-            last_date = first(last_date),
-            total_pixels = first(total_pixels),
-            total_area_km2 = first(total_area_km2),
-            duration = first(duration),
-            simple_fsr_pixels = first(simple_fsr_pixels),
-            simple_fsr_km2 = first(simple_fsr_km2)) %>%
-  ungroup()
-
-nrow(event)
-nrow(df_poly)
 
 # this takes a few minutes
-ecoregions <- st_read("/home/a/data/background/ecoregions/us_eco_l1.gpkg") %>%
+ecoregions <- st_read(ecoregion_file) %>%
   mutate(l1_ecoregion = as.character(l1_ecoregion))
 
 elabs <- ecoregions$l1_ecoregion %>% as.character()
 
-table(daily$lc_name)
+#merging some classes together
+#table(daily$lc_name)
 daily <- filter(daily, lc_name != "Permanent Snow and Ice",
                 lc_name != "Water Bodies",
                 lc_name != "Urban and Built-up Lands",
@@ -96,6 +29,7 @@ daily <- filter(daily, lc_name != "Permanent Snow and Ice",
                 lc_name = replace(lc_name, lc_name == "Evergreen Broadleaf Forests", "Broadleaf Forests"),
                 lc_name = replace(lc_name, lc_name == "Evergreen Needleleaf Forests", "Conifer Forests"))
 
+# this makes the legend colors consistent
 values <- RColorBrewer::brewer.pal(length(unique(daily$lc_name)), "Dark2")
 names(values) <- unique(daily$lc_name)
 
@@ -132,14 +66,14 @@ for(i in ecoregions$NA_L1CODE){
   labs[i] <- filter(ecoregions, NA_L1CODE == i)$l1_ecoregion
 }
 
+#grabbing the legend as an independent object
 leg <- get_legend(ggplot(daily, aes(x=event_day, cum_area_km2, 
                                     group = id)) +
                     geom_line(alpha=0.5, aes(color = lc_name)) +
                     scale_color_manual(values = values,name = "Land Cover")+
                     guides(color=guide_legend(ncol=2)))
 
-#cp[[11]]<- as_ggplot(leg)
-
+# multistage, super advanced plotting process
 x=ggarrange(plotlist=cp, nrow=3, ncol=4) %>%
   annotate_figure(left = text_grob(expression(Area~Burned~(km^2)), rot = 90, size = 20),
                   bottom = text_grob("Days From Ignition", size = 20)) 
---
+++ src/r/get_landcover_and_calculate_daily_stats.R
@@ -0,0 +1,90 @@
+# get landcover and daily stats
+
+# setup ------------------------------------------------------------------------
+libs<- c("raster", "sf", "tidyverse", "fasterize")
+lapply(libs, library, character.only =TRUE)
+
+lc_path <- "/home/a/data/MCD12Q1_mosaics"
+modis_crs <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
+ecoregion_path <- "/home/a/data/background/ecoregions"
+template <- raster(file.path(template_path, "usa_lc_mosaic_2001.tif"))
+
+# loading in data --------------------------------------------------------------
+ecoregions <- st_read(file.path(ecoregion_path, "us_eco_l3.shp")) %>%
+  mutate(NA_L1CODE = as.numeric(NA_L1CODE),
+         l1_ecoregion = str_to_title(NA_L1NAME)) %>%
+  st_transform(crs=modis_crs) %>%
+  dplyr::select(NA_L1CODE, l1_ecoregion)
+
+e_rast <- fasterize(ecoregions, template, field="NA_L1CODE")
+
+# loading in fire event data frame
+df <- read_csv("data/modis_burn_events_00_19.csv") %>%
+  dplyr::select(id,date,x,y) %>%
+  #centering the pixels on the raster cells
+  mutate(x = x + (res(template)[1]/2),
+         y = y - (res(template)[1]/2),
+         year = as.numeric(substr(date, 1,4))) %>%
+  st_as_sf(coords = c("x","y"), crs = modis_crs)
+
+# getting landcover, takes about 5 minutes
+ll<-list()
+cc <- 1
+years <- 2002:2018
+t0<-Sys.time()
+for(y in years){
+  lc <- raster(file.path(lc_path, paste0("usa_lc_mosaic_",y-1,".tif")))
+  ll[[cc]] <- df[df$year == y,] %>%
+    st_as_sf(coords = c("x","y"), crs = modis_crs)%>%
+    mutate(lc = raster::extract(x=lc, y=.))
+  cc<-cc+1
+}
+
+df_lc <- do.call("rbind", ll) %>%
+  mutate(l1_eco = raster::extract(x=e_rast, y=.))
+print(Sys.time() - t0)
+
+#adding the labels to this makes it take forever to write
+st_write(df_lc, 
+         "data/modis_points_w_landcover_02_18.gpkg") 
+
+labels <- st_set_geometry(ecoregions,NULL) %>%
+  mutate(dup = duplicated(NA_L1CODE)) %>%
+  filter(dup == FALSE) %>%
+  dplyr::select(-dup) %>%
+  dplyr::rename(l1_eco = NA_L1CODE)
+
+lc_labels <- read_csv("data/usa_landcover_t1_classes.csv") %>%
+  rename(lc = value, lc_name = name)
+
+daily <- df_lc %>%
+  st_set_geometry(NULL) %>%
+  group_by(id, date) %>%
+  summarise(pixels = n(),
+            l1_eco = getmode(l1_eco),
+            lc = getmode(lc)) %>%
+  ungroup() %>%
+  group_by(id) %>%
+  mutate(cum_pixels = cumsum(pixels),
+         total_pixels = sum(pixels),
+         ignition_date = min(date),
+         last_date = max(date),
+         duration = as.numeric(last_date-ignition_date)+1,
+         simple_fsr_pixels = total_pixels/as.numeric(duration), 
+         simple_fsr_km2 = total_pixels/as.numeric(duration)* 463*463/1000000)%>%
+  ungroup()%>%
+  mutate(daily_area_km2 = pixels * 463*463/1000000,
+         cum_area_km2 = cum_pixels * 463*463/1000000,
+         total_area_km2 = total_pixels * 463*463/1000000,
+         pct_total_area = pixels/total_pixels*100,
+         pct_cum_area = pixels/cum_pixels*100,
+         event_day = as.numeric(date - ignition_date + 1),
+         ratio_area_added_to_average = daily_area_km2/simple_fsr_km2,
+         prior_pixels = cum_pixels-pixels,
+         rel_fsr_per_day = ifelse(prior_pixels>0,pixels/prior_pixels, 0) # need to decide on a value for that
+  ) %>%
+  left_join(labels) %>%
+  left_join(lc_labels) %>%
+  filter(is.na(l1_eco) == F)
+
+write_csv(daily,"data/daily_stats_02-18_asof_20180612.csv")
