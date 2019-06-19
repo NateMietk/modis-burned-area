eb48684a40699716372eafe7168eee76f93d408e
‘admahood’ <admahood@gmail.com>
2019-06-19 15:46:04 -0500

    streamlined latlong/state file

--- src/r/get_lat_long_ignition_points.R
+++ src/r/get_lat_long_ignition_points.R
@@ -8,15 +8,20 @@ getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
-s3_path <- "s3://earthlab-natem/modis-burned-area/MCD64A1/C6/delineated_events"
+
+s3_path <- "s3://earthlab-natem/modis-burned-area/delineated_events"
+cus_path <- "/home/a/data/background/CUS/"
 template_path <- "/home/a/data/MCD12Q1_mosaics/usa_lc_mosaic_2001.tif"
+raw_events_file <- "data/modis_burn_events_00_19.csv"
+
+
 template <- raster(template_path)
 
-cus <- st_read("/home/a/data/background/CUS/") %>%
+cus <- st_read(cus_path) %>%
   st_transform(4326) %>%
-  dplyr::select(state = NAME)
+  dplyr::select(state = NAME) 
 
-ll <- read_csv("data/modis_burn_events_00_19.csv") %>%
+ll <- read_csv(raw_events_file) %>%
   dplyr::select(id,date,x,y) %>%
   #centering the pixels on the raster cells
   mutate(x = x + (res(template)[1]/2),
