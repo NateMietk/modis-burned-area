35b873e6362eae6e21642bfd97b1346ee4bb5b72
‘admahood’ <admahood@gmail.com>
2019-06-19 15:42:15 -0500

    streamlined landcover processing file

--- src/r/landcover_process.r
+++ src/r/landcover_process.r
@@ -3,25 +3,27 @@ library(tidyverse)
 library(gdalUtils)
 library(raster)
 years<- 2001:2017
-dir.create("/home/a/data/MCD12Q1_tifs/")
+local_output <-"/home/a/data/MCD12Q1_tifs"
+local_output_mosaics <- "/home/a/data/MCD12Q1_mosaics"
+dir.create(local_output)
 # getting the tifs
 
 for(y in years){
-lc_files <- list.files(file.path("/home/a/data/MCD12Q1",y), full.names = TRUE)
-dir.create(paste0("/home/a/data/MCD12Q1_tifs/",y))
+lc_files <- list.files(file.path(local_output,y), full.names = TRUE)
+dir.create(file.path(local_output,y))
   for(f in lc_files){
     tile <- str_extract(f, "h\\d{2}v\\d{2}")
-    out_file <- paste0("/home/a/data/MCD12Q1_tifs/",y,"/",y,"_",tile,".tif")
+    out_file <- paste0(local_output,"/",y,"/",y,"_",tile,".tif")
     sds <- f %>%
       gdalUtils::get_subdatasets()
     gdalUtils::gdal_translate(sds[1], dst_dataset = out_file)
    print(paste(y, tile))
   }
 }
-dir.create("/home/a/data/MCD12Q1_mosaics")
+dir.create(local_output_mosaics)
 
 for(y in years){
-  ff <- list.files(file.path("/home/a/data/MCD12Q1_tifs", y), full.names = TRUE)
+  ff <- list.files(file.path(local_output, y), full.names = TRUE)
   rr <- list()
   for(fff in ff){
     rr[[fff]] <- raster(fff)
@@ -30,5 +32,5 @@ for(y in years){
   rr$fun = mean
   rr$na.rm = TRUE
   xx <- do.call(mosaic,rr)
-  writeRaster(xx,paste0("/home/a/data/MCD12Q1_mosaics/","usa_lc_mosaic_",y,".tif"))
+  writeRaster(xx,paste0(local_output_mosaics,"/","usa_lc_mosaic_",y,".tif"))
 }
