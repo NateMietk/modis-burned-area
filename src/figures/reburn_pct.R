9a2240f9ad2da55570dffe95b3311a49539074d7
‘admahood’ <admahood@gmail.com>
2019-07-19 14:30:14 -0600

    updated the reburn fig to include gfa

--- src/figures/reburn_pct.R
+++ src/figures/reburn_pct.R
@@ -10,6 +10,10 @@ mtbs_fires <- st_read("/home/a/data/fire/mtbs/mtbs_perims_DD.shp") %>%
   st_transform(crs = st_crs(modis_fires)) %>%
   mutate(Fire_Name = as.character(Fire_Name) %>% str_to_title())
 
+gfa_fires <- st_read("data/gfa_reburn.gpkg") %>%
+  st_transform(crs = st_crs(modis_fires)) %>%
+  mutate(fire_ID = as.character(fire_ID)) 
+
 bb <- st_bbox(modis_fires)
 
 p1 <- ggplot(modis_fires) +
@@ -30,5 +34,16 @@ p2 <- ggplot(mtbs_fires) +
   ylim(c(bb[2], bb[4])) +
   theme_void()
 
-ggarrange(p1, p2, legend = "bottom") +
-  ggsave("images/reburn_fig.png", dpi = 300)
+p3 <- ggplot(gfa_fires) +
+  geom_sf(aes(fill = fire_ID), alpha = 0.5)+
+  geom_sf(data = mtbs_fires, fill = "transparent") +
+  scale_fill_discrete(name = "GFA ID", breaks = c("23880", "23898","23876", 
+                                                  "23891", "24445", "24490"))+
+  theme_void()+
+ # theme(legend.position = "none") +
+  ggtitle("A. Global Fire Atlas events (57 events)") +
+  xlim(c(bb[1], bb[3])) +
+  ylim(c(bb[2], bb[4]))
+
+ggarrange(p1, p2, p3, legend = "bottom", ncol = 3, nrow=1) +
+  ggsave("images/reburn_fig.png", dpi = 300, width = 12, heigh = 6)
