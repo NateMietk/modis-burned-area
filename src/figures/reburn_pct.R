8b8460cff841b978fbe020cf76d2e58b231bcf17
‘admahood’ <admahood@gmail.com>
2019-07-23 15:12:56 -0600

    polished multipanel figures a bit more

--- src/figures/reburn_pct.R
+++ src/figures/reburn_pct.R
@@ -1,5 +1,5 @@
 # making a figure illustrating reburns
-libs<- c("tidyverse", "sf", "ggpubr")
+libs<- c("tidyverse", "sf", "ggpubr", "ggsn")
 lapply(libs, library, character.only = TRUE)
 
 modis_fires <- st_read("data/events_w_attributes_cus.gpkg") %>%
@@ -14,7 +14,39 @@ gfa_fires <- st_read("data/gfa_reburn.gpkg") %>%
   st_transform(crs = st_crs(modis_fires)) %>%
   mutate(fire_ID = as.character(fire_ID)) 
 
+ignitions <- st_read(file.path("/home/a", "projects", "modis-burned-area",
+                              "data", "events_w_attributes.gpkg")) %>%
+  st_set_geometry(NULL) %>%
+  filter(id == 25211 | id == 29790) %>%
+  dplyr::select(lat=ignition_latitude, long=ignition_longitude,id,
+                ignition_date, last_date) %>%
+  st_as_sf(coords = c("long","lat"), 
+           crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") %>%
+  st_transform(crs = st_crs(modis_fires))
+
 bb <- st_bbox(modis_fires)
+
+ig <- ignitions %>%
+  sf::st_coordinates() %>%
+  as.data.frame() %>%
+  mutate(id = c(25211,29790),
+         X = c(-8040000,-8040000),
+         Y = c(2970000, 2973000))%>%
+  left_join(ignitions %>% st_set_geometry(NULL)) %>%
+  mutate(label = paste0("Start: ", ignition_date, "\nEnd: ", last_date))
+
+mtbs_labs <- mtbs_fires %>%
+  st_set_geometry(NULL) %>%
+  dplyr::select(Fire_Name, Year, StartMonth, StartDay) %>%
+  mutate(label = paste0("Start: ", Year,"-", StartMonth,"-", StartDay),
+         X = c(-8055000, -8055000),
+         Y = c(2995000, 2997000))
+
+north <- data.frame(x = c(-81.27, -81.27), y = c(26.95,26.975)) %>%
+  st_as_sf(coords = c("x","y"), crs=4326) %>%
+  st_transform(crs = st_crs(modis_fires)) %>%
+  st_coordinates() %>%
+  as.data.frame()
 
 p1 <- ggplot(modis_fires) +
   geom_sf(aes(fill = id), alpha = 0.5) +
@@ -23,7 +55,12 @@ p1 <- ggplot(modis_fires) +
   ggtitle("A. FIRED events") +
   xlim(c(bb[1], bb[3])) +
   ylim(c(bb[2], bb[4])) +
-  theme_void()
+  geom_label(data = ig, 
+             aes(x=X,y=Y, label = label, fill = as.factor(id)), fontface = "bold",
+             alpha = 0.5, hjust = "left", size = 2.5, show.legend = FALSE)+
+  theme_bw()+
+  theme(axis.title = element_blank())
+  
 
 p2 <- ggplot(mtbs_fires) +
   geom_sf(aes(fill = Fire_Name), alpha = 0.5) +
@@ -32,18 +69,34 @@ p2 <- ggplot(mtbs_fires) +
   ggtitle("B. MTBS events") +
   xlim(c(bb[1], bb[3])) +
   ylim(c(bb[2], bb[4])) +
-  theme_void()
+  geom_label(data = mtbs_labs, 
+             aes(x=X,y=Y, label = label, fill = as.factor(Fire_Name)), fontface = "bold",
+             alpha = 0.5, hjust = "left", size = 2.5, show.legend = FALSE)+
+  theme_bw()+
+  theme(axis.title = element_blank())
 
 p3 <- ggplot(gfa_fires) +
   geom_sf(aes(fill = fire_ID), alpha = 0.5)+
   geom_sf(data = mtbs_fires, fill = "transparent") +
-  scale_fill_discrete(name = "GFA ID", breaks = c("23880", "23898","23876", 
-                                                  "23891", "24445", "24490"))+
-  theme_void()+
+  scale_fill_discrete(name = "GFA ID", breaks = c("23880","23891", "24490"))+
+  theme_bw()+
+  theme(axis.title = element_blank())+
+  geom_segment(x=north$X[1], xend = north$X[2], 
+               y=north$Y[1], yend = north$Y[2], 
+               arrow = arrow(type = "closed", angle = 15,
+                             length = unit(0.45, "inches")), 
+               lwd = 2, color = "grey30")+
+  geom_text(x=north$X[1], y = north$Y[1], label = "N",
+            fontface="bold", color = "white", angle = -30)+
+  scalebar(gfa_fires, dist = 5, dist_unit = "km", transform = FALSE, 
+           st.dist = .03, st.color = "grey30", box.color = "grey30",  
+           st.bottom = FALSE, box.fill = c("grey30", "white"), 
+           location = "bottomright", st.size = 3)+
  # theme(legend.position = "none") +
-  ggtitle("A. Global Fire Atlas events (57 events)") +
+  ggtitle("C. Global Fire Atlas events (57 events)") +
   xlim(c(bb[1], bb[3])) +
   ylim(c(bb[2], bb[4]))
 
 ggarrange(p1, p2, p3, legend = "bottom", ncol = 3, nrow=1) +
-  ggsave("images/reburn_fig.png", dpi = 300, width = 12, heigh = 6)
+  ggsave("images/reburn_fig.png", dpi = 300, width = 12, height = 6)
+
--- src/figures/rim_fire_comparison_figure.R
+++ src/figures/rim_fire_comparison_figure.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(sf)
 library(ggpubr)
+library(ggsn)
 
 p_path <- "/home/a"
 
@@ -26,31 +27,74 @@ ignition <- st_read(file.path(p_path, "projects", "modis-burned-area",
            crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") %>%
   st_transform(crs = st_crs(FIRED_p))
 
+bb <- st_bbox(FIRED_p)
 
 as.data.frame(sf::st_coordinates(ignition))-> ig
+
+north <- data.frame(x = c(-120.3, -120.3), y = c(38.05,38.08)) %>%
+  st_as_sf(coords = c("x","y"), crs=4326) %>%
+  st_transform(crs = st_crs(FIRED_p)) %>%
+  st_coordinates() %>%
+  as.data.frame()
           
 ggarrange(
   ggplot()+
     geom_sf(data = FIRED_p, aes(fill = as.factor(id))) +
     geom_sf(data = ignition, shape = 8, size = 4, stroke = 1.5) +
     scale_fill_discrete(name = "Fire ID")+
+   
+    theme_bw() + 
+    theme(legend.position = c(0.98,0),
+          legend.justification = c(1,0),
+          legend.background = element_blank(),
+          axis.title = element_blank())+
+    geom_segment(data = FIRED_p,x=north$X[1], xend = north$X[2], 
+                 y=north$Y[1], yend = north$Y[2], 
+                 arrow = arrow(type = "closed", angle = 15,
+                               length = unit(0.65, "inches")), 
+                 lwd = 2, color = "grey30")+
+    geom_text(data = FIRED_p,x=north$X[1]+800, y = north$Y[1]+800, label = "N",
+              fontface="bold", color = "white", angle = -52, size = 6.5)+
     geom_label(data = ig, 
                x=ig$X+2000,y=ig$Y, label = "Estimated Ingition", fontface = "bold",
                alpha = 0.5, hjust = "left")+
-    ggtitle("Rim Fire: FIRED"), 
-  ggplot()+
+    scalebar(FIRED_p, dist = 5, dist_unit = "km",transform = FALSE, 
+             st.dist = .05, st.color = "grey30", box.color = "grey30", 
+             box.fill = c("grey30", "white"), st.bottom = TRUE,
+             location = "topleft")+
+    ggtitle("A: Rim Fire: FIRED"), 
+  
+  ggplot(FIRED_p)+
     geom_sf(data = gfa_p, aes(fill = as.factor(fire_ID))) +
     scale_fill_discrete(name = "Fire ID") +
-    ggtitle("Rim Fire: Global Fire Atlas"),
-  ggplot()+
+    theme_bw() +
+    theme(legend.position = c(0.98,0),
+          legend.justification = c(1,0),
+          legend.background = element_blank(),
+          axis.title = element_blank())+
+    ggtitle("B: Rim Fire: Global Fire Atlas"),
+  ggplot(FIRED_p)+
     geom_sf(data = FIRED_daily, aes(fill = date)) +
     #colors from viridis::viridis(2)
     scale_fill_date(name = "Burn Date",low =  "#FDE725FF", high = "#440154FF") +
-    ggtitle("Rim Fire: FIRED daily"),
-  ggplot()+
+    theme_bw() +
+    theme(legend.position = c(0.98,0),
+          legend.justification = c(1,0),
+          legend.background = element_blank(),
+          axis.title = element_blank())+
+    ggtitle("C: Rim Fire: FIRED daily"),
+  ggplot(FIRED_p)+
     geom_sf(data = mtbs_rim, aes(fill = Fire_Name)) +
-    ggtitle("Rim Fire: MTBS"),
-  ncol =2, nrow=2, labels = "AUTO") +
+    theme_bw() +
+    theme(legend.position = c(0.98,0),
+          legend.justification = c(1,0),
+          legend.background = element_blank())+
+    theme(axis.title = element_blank())+
+    # xlim(c(bb[1], bb[3])) +
+    # ylim(c(bb[2], bb[4])) +
+    
+    ggtitle("D: Rim Fire: MTBS"),
+  ncol =2, nrow=2) +
   ggsave("images/rim_fire_comparison.png", dpi = 600, height = 11, width = 16) +
   ggsave("images/rim_fire_comparison_small.png", 
          dpi = 300, height = 11, width = 16, limitsize = TRUE)
