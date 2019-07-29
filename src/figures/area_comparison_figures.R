3f6063a2b824a53061a12dda3b53017189ee1128
‘admahood’ <admahood@gmail.com>
2019-07-29 15:41:30 -0600

    updated area comparison figures

--- src/figures/area_comparison_figures.R
+++ src/figures/area_comparison_figures.R
@@ -10,7 +10,8 @@ d<- read_csv('data/cm_2019.csv') %>%
   arrange(mtbs_hectares)
 output_table <- data.frame(max_area = NA,
                            r2 = NA,
-                           n=NA)
+                           n=NA,
+                           slope = NA)
 nn <- 50
 
 for(i in 1:nn){
@@ -21,6 +22,7 @@ for(i in 1:nn){
   output_table[i,1] <- f
   output_table[i,2] <- m$r.squared
   output_table[i,3] <- nrow(g)
+  output_table[i,4] <- m$coefficients[2,1]
 }
 
 output_table$std_max_area <- output_table$max_area/e
@@ -45,7 +47,8 @@ p1 <- ggplot(d, aes(modis_ha, mtbs_hectares))+
   geom_smooth(method = "lm") +
   geom_point(alpha = 0.15) +
   geom_abline(slope=1, intercept=0) +
-  theme_pubr() +
+  theme_pubr(base_size = 14) +
+  theme(axis.text = element_text(size = 10)) +
   coord_fixed() +
   xlab("MODIS (hectares)") +
   ylab("MTBS (hectares)") +
@@ -59,19 +62,30 @@ p1 <- ggplot(d, aes(modis_ha, mtbs_hectares))+
 p2 <- ggplot(output_table, aes(max_area, r2)) +
   geom_hline(yintercept = .80, col="grey20", lty=2)+
   geom_line() +
-  geom_line(aes(y= pred), col="red")+
+  #geom_line(aes(y= pred), col="red")+
  # coord_fixed()+
-  theme_pubr() +
-  xlab("Area")+
-  scale_x_continuous(labels = comma)+
-  ylab(expression(R^2)) +
-  annotate("text",  x=100000, y=0.4, label = paste("Linear model for every", 
-                                                    round(max(d$mtbs_hectares)/nn), 
-                                                  "hectare segement.",
-                                                  "Average n = ", mean(output_table$n)))
+  theme_pubr(base_size = 14) +
+  ylab(expression(R^2))+
+  theme(axis.text = element_text(size = 10)) +
+  xlab("MTBS Area (hectares)")+
+  scale_x_continuous(labels = comma) #+
+  # annotate("text",  x=100000, y=0.4, label = paste("Linear model for every", 
+  #                                                   round(max(d$mtbs_hectares)/nn), 
+  #                                                 "hectare segement.",
+  #                                                 "Average n = ", mean(output_table$n)))
 
-ggarrange(p1, p2, nrow = 1, ncol=2, labels = "auto") +
-  ggsave("images/area_analyse.pdf", dpi = 600, height = 6, width = 15)
-ggarrange(p1, p2, nrow = 1, ncol=2, labels = "auto") +
-  ggsave("images/area_analyse.png", dpi = 400, height = 6, width = 15,
+p3 <- ggplot(output_table, aes(max_area, slope)) +
+  geom_hline(yintercept = 1, col="grey20", lty=2)+
+  geom_line() +
+  theme_pubr(base_size = 14) +
+  theme(axis.text = element_text(size = 10))+
+  xlab("MTBS Area (hectares)")+
+  scale_x_continuous(labels = comma)+
+  ylab("Estimate")
+
+ggarrange(p1, p2, p3, nrow = 1, ncol=3, labels = "auto") +
+  ggsave("images/area_analyse.pdf", dpi = 600, height = 4.5, width = 13)
+ggarrange(p1, p2,p3, nrow = 1, ncol=3, labels = "auto") +
+  ggsave("images/area_analyse.png", dpi = 400, height = 4.5, width = 13,
         limitsize = T)
+
