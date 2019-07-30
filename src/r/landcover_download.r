b619c5da9e5b736d2c7070275c967ce048c608bb
‘admahood’ <admahood@gmail.com>
2019-07-30 17:24:04 +0000

    worked more on landcover script

--- src/r/landcover_download.r
+++ src/r/landcover_download.r
@@ -4,21 +4,36 @@ library(tidyverse)
 library(RCurl)
 library(foreach)
 library(doParallel)
-# define tiles
-
-tiles = c("h08v04","h09v04","h10v04","h11v04","h12v04","h13v04",
-          "h08v05","h09v05","h10v05","h11v05","h12v05",
-          "h08v06","h09v06","h10v06","h11v06")
-
-years = 2001:2017
-
-local_data <- "/home/a/data/MCD12Q1"
 
 # input your username and password here as character strings
 uu <- "admahood" 
-pp <-
-
+pp <- "Ulmac323"
+  
 u_p <- paste0(uu,":",pp)
+# define tiles
+url1 <-paste0("https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.006/2001.01.01/")
+
+filenames <- RCurl::getURL(url1, userpwd = u_p)
+# write to a temporary file
+cat(filenames, file = 'tmp.txt')
+
+# read the temporary file as a fixed width text file
+dir_listing1 <- read_fwf('tmp.txt', fwf_empty('tmp.txt'),skip = 37) %>%
+  dplyr::select(X1) %>%
+  filter(str_detect(X1, "\\S{41}.hdf.xml"))%>%
+  mutate(X1 = str_extract(X1, "\\S{41}.hdf"),
+         tiles = substr(X1,18,23))
+#world
+tiles <- dir_listing1$tiles %>% unique
+#coterminous US
+tiles <- c("h08v04","h09v04","h10v04","h11v04","h12v04","h13v04",
+          "h08v05","h09v05","h10v05","h11v05","h12v05",
+          "h08v06","h09v06","h10v06","h11v06")
+
+years = 2001:2016 #2017 disappeared!!
+
+local_data <- "data/MCD12Q1"
+
 
 corz <- detectCores()-1
 registerDoParallel(corz)
@@ -51,8 +66,14 @@ foreach (y = 1:length(years))%dopar%{
   }
 }
 
-# checking to make sure they're all there
+
+# checking to make sure they're all there then writing to s3
 for(y in years){
   print(y)
 print(length(list.files(file.path(local_data,y))))
+system(paste(
+  "aws s3 sync",
+  file.path(local_data,years[y]),
+  file.path("s3://earthlab-natem/modis-burned-area/input/landcover",years[y])
+))
 }
