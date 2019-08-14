0962804bf657e82f5b6efd13d3e404a5155ffcbc
‘admahood’ <admahood@gmail.com>
2019-08-14 21:57:47 +0000

    works in parallel

--- src/r/fix_input.R
+++ src/r/fix_input.R
@@ -13,27 +13,27 @@ tiles_w_fire <- system(" aws s3 ls s3://earthlab-natem/modis-burned-area/delinea
 csvs <- tiles_w_fire[,1]
 twf <- tiles_w_fire[,2]
 
-paste0(tiles_w_fire[,2], collapse= " ")
+# paste0(tiles_w_fire[,2], collapse= " ")
 
-borders <- st_read("~/data/background/world_borders/ne_50m_admin_0_countries.shp") %>%
-  st_transform(crs = st_crs(modis_tiles))
+# borders <- st_read("~/data/background/world_borders/ne_50m_admin_0_countries.shp") %>%
+#   st_transform(crs = st_crs(modis_tiles))
 template_crs <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
 
 reso <- 463.3127
 pix_hectares <- function(p) p * 463.3127*463.3127 * 0.0001
 
 # the biz ===========
-dir.create("data/scrap")
+dir.create("data/scrap", recursive = TRUE)
 dir.create("data/scrap/edge")
 dir.create("data/scrap/notedge")
 
 corz <- detectCores()-1
 registerDoParallel(corz)
-foreach(c = twf, .combine = rbind){
+foreach(c = twf, .combine = rbind)%dopar%{
   #prep
   csvfile <- paste0("data/scrap/",c, ".csv")
   outputn <- paste0("data/scrap/","noedge_",c, ".gpkg")
-  outpute <- paste0("data/scrap/","edge_",c, ".gpkg")
+  outpute <- paste0("data/scrap/","edge_",c, ".csv")
   
   tilenum <- paste0("1",substr(c, 2,3), substr(c,5,6),"0000000") %>% as.numeric
   system(paste0("aws s3 cp ",
@@ -70,22 +70,11 @@ foreach(c = twf, .combine = rbind){
   if(ne>0){
     e <- read_csv(csvfile) %>%
       dplyr::filter(edge == TRUE)%>%
-      dplyr::select(-duration, -tile, -edge) %>%
-      mutate(x = x + (reso/2),
-             y = y - (reso/2),
-             year = as.numeric(substr(date, 1,4)),
-             id = id+tilenum) %>%
-      distinct(x,y,id, .keep_all = T) %>%
-      st_as_sf(coords = c("x","y"), crs = template_crs) %>%
-      group_by(id)%>%
-      st_buffer(dist = 1+(reso/2), endCapStyle = "SQUARE")%>%
-      summarize(start_date = first(date),
-                last_date = last(date),
-                area_burned_ha = pix_hectares(n())) %>%
-      ungroup()
-    st_write(e, outpute, delete_dsn = TRUE)
+      dplyr::select(-duration, -tile, -edge)
+    
+    write_csv(e, outpute)
     system(paste0("aws s3 cp ", outpute," ",
                   "s3://earthlab-natem/modis-burned-area/",
-                  "delineated_events/world/edge/", "edge_",c,".gpkg"))  }
+                  "delineated_events/world/edge/", "edge_",c,".csv"))}  
   unlink(csvfile)
 }
