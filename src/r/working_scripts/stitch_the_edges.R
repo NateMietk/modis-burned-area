3e4b866dc4d316d07b28bfbfdfbb7a0cde014ce2
‘admahood’ <admahood@gmail.com>
2019-09-25 10:34:31 -0600

    l

--- src/r/working_scripts/stitch_the_edges.R
+++ src/r/working_scripts/stitch_the_edges.R
@@ -25,12 +25,12 @@ sspace<-5
 ttime<-11
 
 ss <- sspace * resolution
-tt <- ttime * resolution
+tt <- ttime
 
 #d<-read_csv("data/edges/wh_edges.csv")
 #polygonizing
 t0 <- Sys.time()
-dd <- read_csv("data/edges/wh_edges.csv") %>%
+dd <- read_csv("data/edges/eh_edges.csv") %>%
   mutate(x = x + (resolution/2),
          y = y - (resolution/2)) %>%
   distinct(x,y,id, .keep_all = T) %>%
@@ -42,16 +42,16 @@ dd <- read_csv("data/edges/wh_edges.csv") %>%
   summarize(start_date = first(date),
             end_date = last(date))
   # then make buffer
-print(Sys.time() - t0) # 8 min
+print(Sys.time() - t0) # 8 min wh
 
-# creating the buff # 1 min
+# creating the buff # 1 min wh
 t0 <- Sys.time()
 ddb <- st_buffer(dd, dist = (ss/2)+1)
-st_write(ddb, "data/wh_edge_buffers.gpkg", delete_dsn = TRUE)
+st_write(ddb, "data/eh_edge_buffers.gpkg", delete_dsn = TRUE)
 print(Sys.time() - t0)
 
 t0 <- Sys.time()
-st_overlaps(ddb, sparse = TRUE) -> x # 5 min-ish
+st_overlaps(ddb, sparse = TRUE) -> x # 5 min-ish wh
 print(Sys.time() - t0)
 
 
@@ -59,22 +59,25 @@ dd$overlap <- NA
 for(i in 1:nrow(dd)){
   dd[i,5] <- ifelse(length(x[[i]]) > 0, TRUE, FALSE)
 }
-st_write(dd, "data/wh_edge_polys.gpkg", delete_dsn = TRUE)
-table(dd$overlap)
+st_write(dd, "data/eh_edge_polys.gpkg", delete_dsn = TRUE)
+# table(dd$overlap)
 
-st_write(filter(dd, overlap == FALSE), "data/wh_edges_no_overlaps.gpkg", delete_dsn = TRUE)
-st_write(filter(dd, overlap == TRUE), "data/wh_edges_yes_overlaps.gpkg", delete_dsn = TRUE)
+# st_write(filter(dd, overlap == FALSE), "data/wh_edges_no_overlaps.gpkg", delete_dsn = TRUE)
+# st_write(filter(dd, overlap == TRUE), "data/wh_edges_yes_overlaps.gpkg", delete_dsn = TRUE)
 
-ll <- vector(length = nrow(dd))
-for(i in 1:nrow(x)){
-  ll[i] <- length(x[[i]])
-}
-max(ll)
+# ll <- vector(length = nrow(dd))
+# for(i in 1:nrow(x)){
+#   ll[i] <- length(x[[i]])
+# }
+# max(ll)
 
+t0 <- Sys.time()
+res <- list()
+counter <- 1
 xx <- dd$overlap
 for(i in 1:nrow(dd)){
-  if(dd$overlap[i] == T){
-    print("yay")
+  if(xx[i] == TRUE){
+    print(paste(i,"doin' it", i/nrow(dd) * 100, "%"))
     rows <- x[[i]]
     rows1 <- c(rows, x[rows]) %>% unlist() %>% funique()
     while(length(rows1) != length(rows)){
@@ -83,124 +86,30 @@ for(i in 1:nrow(dd)){
     }
     
     dd_subset <- dd[rows1,]
-    for(i in nrow(subset)){
-      start_range <- dd_subset$start_date[i] - 11
-      end_range <- dd_subset$end_date[i] + 11
-      range <- start_range:end_range
-      ww <- which(as.numeric(dd_subset$start_date) %in% range)
-      ww <- c(ww, which(as.numeric(dd_subset$end_date) %in% range)) %>% funique
-      if(length(ww) != 1){
-        dd_subset[ww,]$id <- dd_subset[ww,]$id %>% min()
+    orig_ids <- dd_subset$id
+    for(j in 1:nrow(dd_subset)){
+      if(orig_ids[j] == dd_subset$id[j]){
+        start_range <- dd_subset$start_date[j] - ttime
+        end_range <- dd_subset$end_date[j] + ttime
+        range <- start_range:end_range
+        ww <- which(as.numeric(dd_subset$start_date) %in% range)
+        ww <- c(ww, which(as.numeric(dd_subset$end_date) %in% range)) %>% funique
+        if(length(ww) > 1){
+          dd_subset[ww,]$id <- dd_subset[ww,]$id %>% min()
+        }
       }
     }
-    
-  }else{print("skip")}
+    xx[rows1] <- FALSE
+    res[[counter]] <- dd_subset
+    counter <- counter + 1
+  }else{print(paste(i,"skipin' it", i/nrow(dd) * 100, "%"))}
 }
-
-
-
-
-
-
-# new_id <- 1
-
-# trying to prepare a vectorized solution
-ddrows <- nrow(dd)
-t0 <- Sys.time()
-vector_out <- rep(NA, ddrows)
-
-ids <- funique(dd[,"id"])
-
-for(i in 1:length(ids)){
-  # if(is.na(vector_out[i])){
-  print(paste(round(i/length(ids)*100, 3), "%"))
-  
-  event <- dd[dd[,"id"]==ids[i],]
-  startrows <- nrow(event)
-
-  if(!is.null(startrows)){
-    id <- ids[i]
-    vector_out[event[,"rownums"]] <- id
-    
-    
-    maxd <- max(event[,"date"])+tt
-    mind <- min(event[,"date"])-tt
-    maxx <- max(event[,"x"])+ss
-    minx <- min(event[,"x"])-ss
-    maxy <- max(event[,"y"])+ss
-    miny <- min(event[,"y"])-ss
-    
-    ddd <- dd[dd[,"date"] >=  mind & 
-                dd[,"date"]<=  maxd &
-                dd[,"x"] >=  minx& 
-                dd[,"x"] <=  maxx &
-                dd[,"y"]>=  miny & 
-                dd[,"y"] <=  maxy, 
-              ]
-    endrows <- nrow(ddd)
-    
-    if(startrows != endrows){
-      maxd <- max(event[,"date"])+100
-      mind <- min(event[,"date"])-100
-      maxx <- max(event[,"x"])+100000
-      minx <- min(event[,"x"])-100000
-      maxy <- max(event[,"y"])+100000
-      miny <- min(event[,"y"])-100000
-      
-      ddd <- dd[dd[,"date"] >=  mind & 
-                  dd[,"date"]<=  maxd &
-                  dd[,"x"] >=  minx& 
-                  dd[,"x"] <=  maxx &
-                  dd[,"y"]>=  miny & 
-                  dd[,"y"] <=  maxy, 
-                ]
-      endrows <- nrow(ddd)
-    while(startrows != endrows){
-      
-      startrows <- sum(na.omit(vector_out == id))
-      
-      for(j in 1:nrow(event)){
-      st_cube <- c(event[j, "x"] - ss, event[j, "x"] + ss,
-                   event[j, "y"] - ss, event[j, "y"] + ss,
-                   event[j, "date"] - tt, event[j, "date"] + tt)
-    
-      rs <- dd[dd[,"date"] >= st_cube[5] & 
-               dd[,"date"]<= st_cube[6]  &
-               dd[,"x"] >= st_cube[1] & 
-               dd[,"x"] <= st_cube[2] &
-               dd[,"y"]>= st_cube[3] & 
-               dd[,"y"] <= st_cube[4], 
-               "rownums"]
-      # ude <- funique(vector_out[rs])
-      # 
-      #   if(length(ude) == 1){
-      #     if(is.na(ude)){
-      #       vector_out[rs] <- id
-      #     }
-      #   }else{
-      #     vector_out[rs] <- ude %>%
-      #       na.omit() %>%
-      #       min()
-      #   }
-       vector_out[rs] <- id
-      
-      endrows <- sum(na.omit(vector_out == id))
-      print(endrows)
-      }
-    }}
-  }
-}
-
 
 print(Sys.time()-t0)
-write_csv(dd, "data/r_events.csv")
-system(paste0("aws s3 cp data/r_events.csv ",
-              "s3://earthlab-natem/modis-burned-area/MCD64A1/C6/event_dataframes/r_events_s",
-              sspace, "_t" ,ttime, ".csv"))
 
-# 
-# crss <- crs(raster(tifs[1]), asText=T)
-# subset <- filter(dd, burn_date<550) %>%
-#   st_as_sf(coords = c("x", "y"),crs = crss)
-#   
-# st_write(subset, "subset.gpkg", delete_dsn = TRUE)
+done <- do.call('rbind', res) %>%
+  group_by(id) %>%
+  summarise(start_date = min(start_date),
+            end_date = max(end_date))
+
+st_write(done,"eh_edges_stitched.gpkg")
